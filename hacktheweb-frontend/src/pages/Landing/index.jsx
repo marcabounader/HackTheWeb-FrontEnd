f3a37f62d24429922cb6dd6a1b84ff6ea4b90432
marcabounader92 <marcabounader92@outlook.com>
2023-09-07 19:24:35 +0300

    Added landing page with signinform modal and footer and content

--- hacktheweb-frontend/src/pages/Landing/index.jsx
+++ hacktheweb-frontend/src/pages/Landing/index.jsx
@@ -1,8 +1,21 @@
-import MatrixAnimation from "../../components/MatrixAnimation";
+import { useState } from "react";
+import Footer from "../../components/Footer";
+import SignInForm from "../../components/Forms/SignInForm";
 
-const Landing = () => {
+const Landing = ({onEnter,onLeave,isLoginModalOpen,handleCloseLoginModal}) => {
     return ( 
-        <MatrixAnimation />
+        <section className="content-wrapper flex flex-col justify-center items-center flex-grow-1 flex-wrap">
+            <SignInForm isOpen={isLoginModalOpen} handleCloseViewModal={handleCloseLoginModal} />
+            <div className="flex flex-col justify-center items-center">
+                <h1>Where offense becomes the ultimate defense!</h1>
+                <p>Here is where you'll uncover the secrets to safeguarding digital fortresses in an ever-evolving digital landscape.</p>
+            </div>
+            <div className="my-5">
+                <input placeholder="Email" className=""/>
+                <button className="btn primary-btn" onMouseEnter={onEnter} onMouseLeave={onLeave}>Start Hacking</button>
+            </div>   
+            <Footer/>
+        </section>
     );
 }
  
