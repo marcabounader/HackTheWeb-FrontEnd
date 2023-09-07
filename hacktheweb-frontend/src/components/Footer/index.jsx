aa8d3c41fbfbb2ad3f25f417d1fbe6117266a309
marcabounader92 <marcabounader92@outlook.com>
2023-09-07 19:06:20 +0300

    Added footer component

---
+++ hacktheweb-frontend/src/components/Footer/index.jsx
@@ -0,0 +1,7 @@
+const Footer = () => {
+    return ( 
+        <footer className="footer text-white text-center">&copy; {new Date().getFullYear()} HackTheWeb</footer>
+    );
+}
+ 
+export default Footer;
