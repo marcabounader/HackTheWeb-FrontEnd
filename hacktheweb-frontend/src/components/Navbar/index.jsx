dc77f88e855adb980962f416625aa400e93e993f
marcabounader92 <marcabounader92@outlook.com>
2023-09-07 19:07:19 +0300

    Added gsap layout effect

--- hacktheweb-frontend/src/components/Navbar/index.jsx
+++ hacktheweb-frontend/src/components/Navbar/index.jsx
@@ -2,27 +2,40 @@ import React from 'react';
 import './Navbar.css'; 
 import mainLogo from '../../assets/logos/main-logo.svg';
 import { useLocation } from 'react-router-dom';
+import { gsap } from 'gsap';
+import { useLayoutEffect, useRef } from 'react';
 
-const Navbar = () => {
+const Navbar = ({onEnter,onLeave,handleOpenLoginModal}) => {
     const location = useLocation();
     const isLanding = location.pathname === '/';
+    const logoRef = useRef();
+    const btnRef = useRef();
+
+    useLayoutEffect(() => {
+      gsap.to(logoRef.current, {
+        rotation: "+=360"
+      });
+      gsap.to(btnRef.current, {
+        rotation: "+=360"
+      });
+    });
 
     return (
     
         <div className="navbar flex flex-row justify-between items-center px-20 py-10 bg-">
-        <div className="logo-container">
+        <div className="logo-container" ref={logoRef}>
             <img src={mainLogo} alt="Main Logo" />
         </div>
         <div className="buttons-container">
-            <button className="btn secondary-btn">Leaderboard</button>
+            <button className="btn" onMouseEnter={onEnter} onMouseLeave={onLeave}>Leaderboard</button>
             {
                 isLanding ? 
                     (
-                        <button className="btn primary-btn">Login/Register</button>
+                        <button ref={btnRef} className="btn primary-btn" onClick={handleOpenLoginModal} onMouseEnter={onEnter} onMouseLeave={onLeave}>Login/Register</button>
                     )
                 :
                     (
-                        <button className="btn primary-btn">Profile</button>
+                        <button ref={btnRef} className="btn primary-btn"  onMouseEnter={onEnter} onMouseLeave={onLeave}>Profile</button>
 
                     )
             }
