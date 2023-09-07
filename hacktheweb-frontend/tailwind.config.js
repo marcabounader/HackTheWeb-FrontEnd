b28ae750dd6e11366d4d489b318e2df77c3524a9
marcabounader92 <marcabounader92@outlook.com>
2023-09-07 19:03:06 +0300

    Added tailwind config main-bgf

--- hacktheweb-frontend/tailwind.config.js
+++ hacktheweb-frontend/tailwind.config.js
@@ -3,7 +3,11 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'bg-main': '#212529'
+      },
+    },
   },
   plugins: [],
 };
