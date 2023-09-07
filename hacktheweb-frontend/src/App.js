a8f96ebc923a261a461350f244ab221d148347c5
marcabounader92 <marcabounader92@outlook.com>
2023-09-07 14:15:25 +0300

    Initialized the react project and added dependencies(redux,styled-components,tailwind,react-router)

---
+++ hacktheweb-frontend/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Landing from './pages/Landing';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+   <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+        </Routes>
+    </Router>
+  );
+}
+
+export default App;
