a063d2ff980a93a33ddc43fe7f16cf52a1c1c8e2
marcabounader92 <marcabounader92@outlook.com>
2023-09-07 19:06:53 +0300

    Added custom input and text area

---
+++ hacktheweb-frontend/src/components/Inputs/CustomInput.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const CustomInput = ({
+  name,
+  label,
+  value,
+  onChange,
+  type = 'text',
+  className,
+  error,
+}) => {
+  return (
+    <div className="input-container flex flex-col gap-2 font-normal">
+      <label className=".label">
+        {label}
+      </label>
+      <input
+        name={name}
+        value={value}
+        className={` shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+          error && 'border-red-500'
+        } ${className}`}
+        type={type}
+        onChange={onChange}
+      />
+    </div>
+  );
+};
+
+export default CustomInput;
