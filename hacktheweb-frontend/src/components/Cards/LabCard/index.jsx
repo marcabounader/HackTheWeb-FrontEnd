b009582938cf1c4a04d2be90116f041383f439d2
marcabounader92 <marcabounader92@outlook.com>
2023-09-08 19:30:27 +0300

    Added lab card content styling

--- hacktheweb-frontend/src/components/Cards/LabCard/index.jsx
+++ hacktheweb-frontend/src/components/Cards/LabCard/index.jsx
@@ -1,6 +1,16 @@
-const LabCard = () => {
+const LabCard = ({lab}) => {
+    console.log(lab);
     return ( 
-        <h1>labcard</h1>
+        <div className="flex w-[306px] h-[157px] p-[10px] flex-col align-start justify-between shadow-md bg-black rounded-[10px]">
+            <div className="flex flex-row">
+                <img className="w-[60px] h-[50px]" src={lab.icon_url} alt="lab image"/>
+                <h4 className=" flex-grow self-stretch text-center">{lab.name}</h4>
+            </div>
+            <div className="flex flex-row justify-around">
+                <p className=" text-green-400">{lab.difficulty_info.difficulty}</p>
+                <p>Score: {lab.score}</p>
+            </div>
+        </div>
      );
 }
  
