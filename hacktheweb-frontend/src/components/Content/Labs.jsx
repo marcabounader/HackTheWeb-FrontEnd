d155ac0a1bc5522191b58363e758fe80ebd7388d
marcabounader92 <marcabounader92@outlook.com>
2023-09-08 19:31:03 +0300

    Added lab content fetch with card render

--- hacktheweb-frontend/src/components/Content/Labs.jsx
+++ hacktheweb-frontend/src/components/Content/Labs.jsx
@@ -1,6 +1,31 @@
-const Labs = () => {
+import { useEffect, useState } from "react";
+import { getLabs } from "../../helpers/user.helpers";
+import { useNavigate } from "react-router-dom";
+import LabCard from "../Cards/LabCard";
+
+const Labs = ({token}) => {
+    const [labs,setLabs]=useState([]);
+    const navigate=useNavigate();
+    useEffect(() => {
+        const fetchLabs = async () => {
+          const {data , errors, message} = await getLabs(token);
+          if(data.message=="Unauthenticated."){
+            navigate("/");
+          }else if (data && data.labs) {
+            setLabs([...data.labs]);
+          }
+
+        };
+    
+        fetchLabs();
+      }, []);
     return ( 
-        <h1>labs</h1>
+        <>
+
+            {labs && labs.map((lab, index) => (
+                <LabCard lab={lab} key={index}/>
+            ))}
+        </>
      );
 }
  
