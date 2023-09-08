6babb39ce54665537534df0a82a9d2dcc1400402
marcabounader92 <marcabounader92@outlook.com>
2023-09-08 16:40:13 +0300

    Fixed circle visibility into app.js

--- hacktheweb-frontend/src/App.js
+++ hacktheweb-frontend/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Landing from './pages/Landing';
 import Navbar from './components/Navbar';
 import { gsap } from 'gsap';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import UserDashboard from './pages/UserDashboard';
 
 
@@ -13,6 +13,11 @@ function App() {
   const handleCloseLoginModal = () => setIsLoginModalOpen(false);
   const handleOpenRegisterModal = () => setIsRegisterModalOpen(true);
   const handleCloseRegisterModal = () => setIsRegisterModalOpen(false);
+  const [areCirclesVisible, setAreCirclesVisible] = useState(true);
+
+  const circleRefs = useRef([]);
+
+  circleRefs.current = [];
 
   const onEnter = ({ currentTarget }) => {
     if(currentTarget.classList.contains("primary-btn")){
@@ -32,13 +37,39 @@ function App() {
 
     }
   };
+  
+  const addCircleRef = ref => {
+    if (ref) {
+      circleRefs.current.push(ref);
+    }    
+  };
+  useEffect(() => {
+    const { innerWidth, innerHeight } = window;
+    circleRefs.current.forEach(ref => ref.moveTo(innerWidth / 2, innerHeight / 2));
+    
+    const onMove = ({ clientX, clientY }) => {
+      const cursorElement = document.elementFromPoint(clientX, clientY);
+      const isCursorOverInputOrButton =
+      cursorElement &&
+      (cursorElement.tagName === "INPUT" || cursorElement.tagName === "BUTTON");
 
+      circleRefs.current.forEach((ref) => 
+      {
+        ref.moveTo(clientX, clientY);
+      });
+    };
+    
+    
+    window.addEventListener("pointermove", onMove);
+    
+    return () => window.removeEventListener("pointermove", onMove);
+  }, [areCirclesVisible]);
   return (
    <Router>
         <Navbar onEnter={onEnter} onLeave={onLeave} handleOpenLoginModal={handleOpenLoginModal} handleOpenRegisterModal={handleOpenRegisterModal}/>
         <Routes>
-          <Route path="/" element={<Landing isLoginModalOpen={isLoginModalOpen} handleCloseLoginModal={handleCloseLoginModal} isRegisterModalOpen={isRegisterModalOpen} handleCloseRegisterModal={handleCloseRegisterModal} handleOpenRegisterModal={handleOpenRegisterModal} handleOpenLoginModal={handleOpenLoginModal} onEnter={onEnter} onLeave={onLeave}/>} />
-          <Route path="/user-dashboard" element={<UserDashboard/>}/>
+          <Route path="/" element={<Landing addCircleRef={addCircleRef} isLoginModalOpen={isLoginModalOpen} handleCloseLoginModal={handleCloseLoginModal} isRegisterModalOpen={isRegisterModalOpen} handleCloseRegisterModal={handleCloseRegisterModal} handleOpenRegisterModal={handleOpenRegisterModal} handleOpenLoginModal={handleOpenLoginModal} onEnter={onEnter} onLeave={onLeave} areCirclesVisible={areCirclesVisible}/>} />
+          <Route path="/user-dashboard" element={<UserDashboard addCircleRef={addCircleRef} areCirclesVisible={areCirclesVisible}/>}/>
         </Routes>
     </Router>
   );
--- hacktheweb-frontend/src/pages/Landing/index.jsx
+++ hacktheweb-frontend/src/pages/Landing/index.jsx
@@ -5,7 +5,7 @@ import RegisterForm from "../../components/Forms/RegisterForm";
 import Circle from "../../components/Objects/circle";
 import "./landing.css"
 
-const Landing = ({onEnter,onLeave,isLoginModalOpen,handleOpenLoginModal, handleCloseLoginModal,isRegisterModalOpen,handleCloseRegisterModal,handleOpenRegisterModal}) => {
+const Landing = ({addCircleRef,onEnter,onLeave,isLoginModalOpen,handleOpenLoginModal, handleCloseLoginModal,isRegisterModalOpen,handleCloseRegisterModal,handleOpenRegisterModal,areCirclesVisible}) => {
     const circleRefs = useRef([]);
     const [myEmail,setMyEmail]=useState('');
     circleRefs.current = [];
@@ -15,32 +15,19 @@ const Landing = ({onEnter,onLeave,isLoginModalOpen,handleOpenLoginModal, handleC
       const { value} = e.target;
       setMyEmail(value);
     }
-
-    useEffect(() => {
-      const { innerWidth, innerHeight } = window;
-      circleRefs.current.forEach(ref => ref.moveTo(innerWidth / 2, innerHeight / 2));
-      
-      const onMove = ({ clientX, clientY }) => {      
-        circleRefs.current.forEach(ref => ref.moveTo(clientX, clientY));
-      };
-      
-      window.addEventListener("pointermove", onMove);
-      
-      return () => window.removeEventListener("pointermove", onMove);
-    }, []);
     
-    const addCircleRef = ref => {
-      if (ref) {
-        circleRefs.current.push(ref);
-      }    
-    };
+    const circles= [];
+    
+    if (areCirclesVisible) {
+      circles.push(<Circle size="sm" ref={addCircleRef} delay={0} key="circle-sm" />);
+      circles.push(<Circle size="md" ref={addCircleRef} delay={0.1} key="circle-md" />);
+      circles.push(<Circle size="lg" ref={addCircleRef} delay={0.2} key="circle-lg" />);
+    }
     return ( 
         <section className="main-wrapper flex flex-col justify-center items-center flex-grow-1 flex-wrap">
             <LoginForm isOpen={isLoginModalOpen} handleCloseViewModal={handleCloseLoginModal} handleOpenRegisterModal={handleOpenRegisterModal}/>
             <RegisterForm isOpen={isRegisterModalOpen} handleCloseViewModal={handleCloseRegisterModal} handleOpenLoginModal={handleOpenLoginModal} setMyEmail={setMyEmail} myEmail={myEmail}/>
-            <Circle size="sm" ref={addCircleRef} delay={0} />
-            <Circle size="md" ref={addCircleRef} delay={0.1} />
-            <Circle size="lg" ref={addCircleRef} delay={0.2} />
+            {circles}
             <div className="flex flex-col justify-center items-center">
                 <h1>Where offense becomes the ultimate defense!</h1>
                 <p>Here is where you'll uncover the secrets to safeguarding digital fortresses in an ever-evolving digital landscape.</p>
