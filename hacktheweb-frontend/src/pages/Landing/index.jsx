27d3a25d3f48495407aab9b43c9622b858b9f23b
marcabounader92 <marcabounader92@outlook.com>
2023-09-08 13:22:25 +0300

    Added circlle func to landing

--- hacktheweb-frontend/src/pages/Landing/index.jsx
+++ hacktheweb-frontend/src/pages/Landing/index.jsx
@@ -1,13 +1,40 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Footer from "../../components/Footer";
 import LoginForm from "../../components/Forms/LoginForm";
 import RegisterForm from "../../components/Forms/RegisterForm";
-
+import Circle from "../../components/Objects/circle";
+import "./landing.css"
 const Landing = ({onEnter,onLeave,isLoginModalOpen,handleOpenLoginModal, handleCloseLoginModal,isRegisterModalOpen,handleCloseRegisterModal,handleOpenRegisterModal}) => {
+    const circleRefs = useRef([]);
+
+    circleRefs.current = [];
+    
+    useEffect(() => {
+      const { innerWidth, innerHeight } = window;
+      circleRefs.current.forEach(ref => ref.moveTo(innerWidth / 2, innerHeight / 2));
+      
+      const onMove = ({ clientX, clientY }) => {      
+        circleRefs.current.forEach(ref => ref.moveTo(clientX, clientY));
+      };
+      
+      window.addEventListener("pointermove", onMove);
+      
+      return () => window.removeEventListener("pointermove", onMove);
+    }, []);
+    
+    const addCircleRef = ref => {
+      if (ref) {
+        circleRefs.current.push(ref);
+      }    
+    };
+    
     return ( 
         <section className="content-wrapper flex flex-col justify-center items-center flex-grow-1 flex-wrap">
             <LoginForm isOpen={isLoginModalOpen} handleCloseViewModal={handleCloseLoginModal} handleOpenRegisterModal={handleOpenRegisterModal}/>
             <RegisterForm isOpen={isRegisterModalOpen} handleCloseViewModal={handleCloseRegisterModal} handleOpenLoginModal={handleOpenLoginModal}/>
+            <Circle size="sm" ref={addCircleRef} delay={0} />
+            <Circle size="md" ref={addCircleRef} delay={0.1} />
+            <Circle size="lg" ref={addCircleRef} delay={0.2} />
             <div className="flex flex-col justify-center items-center">
                 <h1>Where offense becomes the ultimate defense!</h1>
                 <p>Here is where you'll uncover the secrets to safeguarding digital fortresses in an ever-evolving digital landscape.</p>
