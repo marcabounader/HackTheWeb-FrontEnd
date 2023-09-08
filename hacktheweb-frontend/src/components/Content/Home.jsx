69db0b5630201b307a41082022852bd8e2808bbd
marcabounader92 <marcabounader92@outlook.com>
2023-09-08 18:24:25 +0300

    Added home content functions and statistics card

--- hacktheweb-frontend/src/components/Content/Home.jsx
+++ hacktheweb-frontend/src/components/Content/Home.jsx
@@ -1,6 +1,37 @@
-const Home = () => {
+import { useEffect, useState } from "react";
+import StatisticsCard from "../Cards/StatisticsCard";
+import { getStatistics } from "../../helpers/user.helpers";
+import { useNavigate } from "react-router-dom";
+
+const Home = ({token}) => {
+    const [statistics,setStatistics]=useState('');
+    const navigate=useNavigate();
+    useEffect(() => {
+        const fetchStatistics = async () => {
+          const {data , errors, message} = await getStatistics(token);
+          if (data) {
+            const {message,...statistics_temp} = data;
+            setStatistics(statistics_temp);
+          }
+          if(data.message=="Unauthenticated"){
+            navigate("/");
+          }
+        };
+    
+        fetchStatistics();
+      }, []);
     return ( 
-        <h1>home</h1>
+        <>
+        {statistics && 
+            Object.keys(statistics).map((key) => {
+                const value = statistics[key];
+                const formattedKey = key.replace(/_/g, ' ').replace(/\b\w/g, (c) => c.toUpperCase());
+                return (
+                  <StatisticsCard name={formattedKey} statistic={value} key={key}/>
+                );
+        })}
+        </>
+
      );
 }
  
