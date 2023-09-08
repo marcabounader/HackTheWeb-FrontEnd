8ea224020e55288a074c80bb024b8caa4df39dd9
marcabounader92 <marcabounader92@outlook.com>
2023-09-08 18:25:13 +0300

    Added get statistics

---
+++ hacktheweb-frontend/src/helpers/user.helpers.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+const baseUrl = 'http://localhost:8000/api/';
+
+
+
+async function getStatistics(token) {
+    try {
+      const res = await axios.get(`${baseUrl}hacker/statistics`, {headers: { Authorization: `Bearer ${token}` }});
+      if (res.status === 200) {
+        const data = res.data;
+        return { data };
+      }
+    } catch (error) {
+      const {
+        response: {
+          data: { message, errors },
+        },
+      } = error;
+  
+      if (errors) {
+        const errorMessages = Object.keys(errors).map((key) => {
+          const firstError = errors[key][0];
+          if (firstError) {
+            return firstError;
+          }
+        });
+        return { errorMessages };
+      }
+      return { message };
+    }
+  }
+
+  export {getStatistics};
