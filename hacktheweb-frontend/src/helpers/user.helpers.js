d2debc4a5c7f4494c4349e07906e8588addda59a
marcabounader92 <marcabounader92@outlook.com>
2023-09-09 19:26:45 +0300

    Added get active lab, get com labs, stop lab helpers

--- hacktheweb-frontend/src/helpers/user.helpers.js
+++ hacktheweb-frontend/src/helpers/user.helpers.js
@@ -57,5 +57,85 @@ async function getLabs(token) {
     return { message };
   }
 }
+async function getActiveLabs(token) {
+  try {
+    const res = await axios.get(`${baseUrl}hacker/get-active-labs`, {headers: { Authorization: `Bearer ${token}` }});
+    if (res.status === 200) {
+      const data = res.data;
+      return { data };
+    }
+  } catch (error) {
+    const {
+      response: {
+        data: { message, errors },
+      },
+    } = error;
 
-  export {getStatistics,getLabs};
+    if (errors) {
+      const errorMessages = Object.keys(errors).map((key) => {
+        const firstError = errors[key][0];
+        if (firstError) {
+          return firstError;
+        }
+      });
+      return { errorMessages };
+    }
+    return { message };
+  }
+}
+async function getCompletedLabs(token) {
+  try {
+    const res = await axios.get(`${baseUrl}hacker/get-completed-labs`, {headers: { Authorization: `Bearer ${token}` }});
+    if (res.status === 200) {
+      const data = res.data;
+      return { data };
+    }
+  } catch (error) {
+    const {
+      response: {
+        data: { message, errors },
+      },
+    } = error;
+
+    if (errors) {
+      const errorMessages = Object.keys(errors).map((key) => {
+        const firstError = errors[key][0];
+        if (firstError) {
+          return firstError;
+        }
+      });
+      return { errorMessages };
+    }
+    return { message };
+  }
+}
+
+async function stopLab(token,project_name) {
+  try {
+    const modifiedString = project_name.replace(/_\d+$/, '');
+
+    const res = await axios.delete(`${baseUrl}hacker/stop-user-lab/${modifiedString}`, {headers: { Authorization: `Bearer ${token}` }});
+    if (res.status === 200) {
+      const data = res.data;
+      return { data };
+    }
+  } catch (error) {
+    const {
+      response: {
+        data: { message, errors },
+      },
+    } = error;
+
+    if (errors) {
+      const errorMessages = Object.keys(errors).map((key) => {
+        const firstError = errors[key][0];
+        if (firstError) {
+          return firstError;
+        }
+      });
+      return { errorMessages };
+    }
+    return { message };
+  }
+}
+  export {getStatistics,getLabs,getActiveLabs,getCompletedLabs,stopLab};
