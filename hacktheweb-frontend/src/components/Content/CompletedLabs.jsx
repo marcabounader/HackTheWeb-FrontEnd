fd61de67d1b567fcfe849593a13f41f0db0c1401
marcabounader92 <marcabounader92@outlook.com>
2023-09-09 19:25:23 +0300

    Added completed labs lab card

--- hacktheweb-frontend/src/components/Content/CompletedLabs.jsx
+++ hacktheweb-frontend/src/components/Content/CompletedLabs.jsx
@@ -1,6 +1,16 @@
+import { useSelector } from "react-redux";
+import LabCard from "../Cards/LabCard";
+
 const CompletedLabs = () => {
+    const labs = useSelector((state) => state.labs.completedLabs);
     return ( 
-        <h1>completed</h1>
+        <>
+        <h1 className=" text-start w-full">Completed Labs</h1>
+            {labs && labs.map((lab, index) => (
+                <LabCard lab={lab} key={index}/>
+                
+            ))}
+        </>
      );
 }
  
