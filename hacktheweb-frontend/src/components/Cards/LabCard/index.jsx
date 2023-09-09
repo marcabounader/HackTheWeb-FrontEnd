fb26651637a889ef7f8fa985340407b921941c0c
marcabounader92 <marcabounader92@outlook.com>
2023-09-09 13:56:24 +0300

    Added lab modal handling

--- hacktheweb-frontend/src/components/Cards/LabCard/index.jsx
+++ hacktheweb-frontend/src/components/Cards/LabCard/index.jsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
+import LabModal from "../../Modals/LabModal";
+
 const LabCard = ({lab}) => {
-    console.log(lab);
+    const [showLab,setShowLab]=useState(false);
+    const handleOpenShowLab = () => setShowLab(true);
+    const handleCloseShowLab = () => setShowLab(false);
+    
     return ( 
-        <div className="flex w-[352] h-[157px] p-[10px] flex-col align-start justify-around shadow-md bg-bg-card rounded-[10px]">
+        <>
+        <LabModal isOpen={showLab} handleCloseViewModal={handleCloseShowLab} lab={lab}/>
+        <div className=" cursor-pointer flex w-[352] h-[157px] p-[10px] flex-col align-start justify-around shadow-md bg-bg-card rounded-[10px]" onClick={handleOpenShowLab}>
             <div className="flex flex-row">
                 <img className="w-[60px] h-[50px]" src={lab.icon_url} alt="lab image"/>
                 <h6 className=" flex-grow self-stretch text-center capitalize">{lab.name}</h6>
@@ -11,6 +19,7 @@ const LabCard = ({lab}) => {
                 <p>Reward: {lab.reward}</p>
             </div>
         </div>
+        </>
      );
 }
  
