d08b1ddd8ab504f0867aa5f2a9c359ae65add2f2
marcabounader92 <marcabounader92@outlook.com>
2023-09-09 12:04:37 +0300

    Fixed lab and statistics card css

--- hacktheweb-frontend/src/components/Cards/LabCard/index.jsx
+++ hacktheweb-frontend/src/components/Cards/LabCard/index.jsx
@@ -1,14 +1,14 @@
 const LabCard = ({lab}) => {
     console.log(lab);
     return ( 
-        <div className="flex w-[306px] h-[157px] p-[10px] flex-col align-start justify-between shadow-md bg-black rounded-[10px]">
+        <div className="flex w-[352] h-[157px] p-[10px] flex-col align-start justify-around shadow-md bg-bg-card rounded-[10px]">
             <div className="flex flex-row">
                 <img className="w-[60px] h-[50px]" src={lab.icon_url} alt="lab image"/>
-                <h4 className=" flex-grow self-stretch text-center">{lab.name}</h4>
+                <h6 className=" flex-grow self-stretch text-center capitalize">{lab.name}</h6>
             </div>
-            <div className="flex flex-row justify-around">
+            <div className="flex flex-row justify-between">
                 <p className=" text-green-400">{lab.difficulty_info.difficulty}</p>
-                <p>Score: {lab.score}</p>
+                <p>Reward: {lab.reward}</p>
             </div>
         </div>
      );
--- hacktheweb-frontend/src/components/Cards/StatisticsCard/index.jsx
+++ hacktheweb-frontend/src/components/Cards/StatisticsCard/index.jsx
@@ -1,8 +1,8 @@
 const StatisticsCard = ({name,statistic}) => {
     return ( 
-        <div className="flex w-[306px] h-[157px] p-[10px] flex-col items-center gap-[10px] shadow-md bg-black rounded-[10px]">
+        <div className="flex w-[352px] h-[157px] p-[10px] flex-col justify-around items-center gap-[10px] shadow-md bg-bg-card rounded-[10px]">
             <h4>{name}</h4>
-            <p>{statistic}</p>
+            <p className=" text-5xl">{statistic}</p>
         </div>       
     );
 }
