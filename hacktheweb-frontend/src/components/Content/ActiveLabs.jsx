3ccec1ab07416c445585e0c536ad929b165fb688
marcabounader92 <marcabounader92@outlook.com>
2023-09-09 19:25:13 +0300

    Added active lab lab card

--- hacktheweb-frontend/src/components/Content/ActiveLabs.jsx
+++ hacktheweb-frontend/src/components/Content/ActiveLabs.jsx
@@ -1,7 +1,23 @@
+import { useSelector } from "react-redux";
+import LabCard from "../Cards/LabCard";
+
 const ActiveLabs = () => {
-    return ( 
-        <h1>active labs</h1>
-    );
-}
- 
+  const labs = useSelector((state) => state.labs.activeLabs);
+  console.log(labs);
+  const isActive = true;
+
+  return (
+    <>
+      <h1 className="text-start w-full">Active Labs</h1>
+      {labs && labs.length > 0 ? (
+        labs.map((lab, index) => (
+          <LabCard lab={lab} key={index} isActive={isActive} />
+        ))
+      ) : (
+        <p>No active labs available.</p>
+      )}
+    </>
+  );
+};
+
 export default ActiveLabs;
