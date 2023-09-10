82d95bbaee62c38b013df40c2b9416a9092182a1
marcabounader92 <marcabounader92@outlook.com>
2023-09-11 01:23:14 +0300

    Altered active labs to lab

--- hacktheweb-frontend/src/components/Content/ActiveLabs.jsx
+++ hacktheweb-frontend/src/components/Content/ActiveLabs.jsx
@@ -2,17 +2,15 @@ import { useSelector } from "react-redux";
 import LabCard from "../Cards/LabCard";
 
 const ActiveLabs = () => {
-  const labs = useSelector((state) => state.labs.activeLabs);
-  console.log(labs);
-  const isActive = true;
+  const labs = useSelector((state) => state.labs.labs);
 
   return (
     <>
       <h1 className="text-start w-full">Active Labs</h1>
       {labs && labs.length > 0 ? (
-        labs.map((lab, index) => (
-          <LabCard lab={lab} key={index} isActive={isActive} />
-        ))
+        labs
+          .filter((lab) => lab.isActive)
+          .map((lab, index) => <LabCard lab={lab} key={index} />)
       ) : (
         <p>No active labs available.</p>
       )}
