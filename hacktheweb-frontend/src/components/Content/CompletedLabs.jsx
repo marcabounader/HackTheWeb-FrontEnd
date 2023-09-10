a136503ad4c864b5546daf1595dd0eee7b53d8b7
marcabounader92 <marcabounader92@outlook.com>
2023-09-11 01:23:24 +0300

    Altered completed labs to labs

--- hacktheweb-frontend/src/components/Content/CompletedLabs.jsx
+++ hacktheweb-frontend/src/components/Content/CompletedLabs.jsx
@@ -2,14 +2,17 @@ import { useSelector } from "react-redux";
 import LabCard from "../Cards/LabCard";
 
 const CompletedLabs = () => {
-    const labs = useSelector((state) => state.labs.completedLabs);
+    const labs = useSelector((state) => state.labs.labs);
     return ( 
         <>
         <h1 className=" text-start w-full">Completed Labs</h1>
-            {labs && labs.map((lab, index) => (
-                <LabCard lab={lab} key={index}/>
-                
-            ))}
+            {labs && labs.length > 0 ? (
+                labs
+                .filter((lab) => lab.isComplete)
+                .map((lab, index) => <LabCard lab={lab} key={index} />)
+            ) : (
+                <p>No active labs available.</p>
+            )}
         </>
      );
 }
