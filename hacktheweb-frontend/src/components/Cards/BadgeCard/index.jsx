9a26847c6254c4f1b368ef879ff3cb5e48f38eb3
marcabounader92 <marcabounader92@outlook.com>
2023-09-11 01:22:16 +0300

    Added badge card content

---
+++ hacktheweb-frontend/src/components/Cards/BadgeCard/index.jsx
@@ -0,0 +1,28 @@
+import './BadgeCard.css';
+const BadgeCard = ({badge}) => {
+    return ( 
+        <div className="flex w=[277px] h=[370px] p-[10px] flex-col items-center gap-[10px] bg-bg-card rounded-xl shadow-lg">
+            <h4>{badge.name}</h4>
+            { badge.category_id == 3 ? (
+                <img src={badge.icon_url} alt={`${badge.name} icon`} className='w-[139px] h-[137px] bronze-badge'/>
+            )
+            :
+            (
+                <>
+                { badge.category_id == 2 ? (
+                <img src={badge.icon_url} alt={`${badge.name} icon`} className='w-[139px] h-[137px] silver-badge'/>
+                )
+                :
+                (
+                    <img src={badge.icon_url} alt={`${badge.name} icon`} className='w-[139px] h-[137px] gold-badge'/>
+                )
+                }
+                </>
+            )
+            }
+
+        </div>
+    );
+}
+ 
+export default BadgeCard;
