01eb66a9e8a4edbea953aec0054092101ae83a6a
marcabounader92 <marcabounader92@outlook.com>
2023-09-11 01:22:59 +0300

    Added achievements content with badgeCard

--- hacktheweb-frontend/src/components/Content/Achievements.jsx
+++ hacktheweb-frontend/src/components/Content/Achievements.jsx
@@ -1,6 +1,18 @@
+import { useSelector } from "react-redux";
+import BadgeCard from "../Cards/BadgeCard";
+
 const Achievements = () => {
+    const badges = useSelector((state) => state.labs.badges);
+
     return ( 
-        <h1>Achievements</h1>
+        <>
+        <h1 className="text-start w-full">Active Labs</h1>
+        {badges && badges.length > 0 ? (
+        badges.map((badge, index) => <BadgeCard badge={badge} key={index} />)
+        ) : (
+          <p>No Badges available.</p>
+        )}
+      </>
      );
 }
  
