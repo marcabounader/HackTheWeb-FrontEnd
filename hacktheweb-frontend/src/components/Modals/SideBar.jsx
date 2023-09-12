6f910fba4f7a4743b3a51711887207806599a793
marcabounader92 <marcabounader92@outlook.com>
2023-09-12 14:36:44 +0300

    Added sidebar logout

--- hacktheweb-frontend/src/components/Modals/SideBar.jsx
+++ hacktheweb-frontend/src/components/Modals/SideBar.jsx
@@ -1,10 +1,27 @@
 import Modal from 'react-modal';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSquareXmark } from '@fortawesome/free-solid-svg-icons';
+import { logOut } from '../../helpers/auth.helpers';
+import { resetUserState } from '../../slices/userSlice';
+import { resetLabState } from '../../slices/labSlice';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 
 Modal.setAppElement('#root');
 
 const SideBar = ({isOpen,handleCloseViewModal,handleOpenProfile, handleOpenPassword ,onEnter,onLeave}) => {
+    const user = useSelector((state) => state.user);
+    const { token } = user;
+    const navigate=useNavigate();
+    const dispatch=useDispatch();
+    const handleLogOut = async () =>{
+        const {data,error,errorMessages} = await logOut(token);
+        if (data && data.message=="Successfully logged out"){
+            dispatch(resetUserState()); // Reset the user state
+            dispatch(resetLabState()); // Reset the labs state
+                  navigate('/');
+        }
+    }
     return ( 
         <Modal
         isOpen={isOpen}
@@ -15,6 +32,8 @@ const SideBar = ({isOpen,handleCloseViewModal,handleOpenProfile, handleOpenPassw
             <FontAwesomeIcon icon={faSquareXmark} onClick={handleCloseViewModal} className=' self-end w-[30px] h-[30px]' fill="var(--text-and-secondary, #A4B1CD)"/>            
             <button onMouseEnter={onEnter} onMouseLeave={onLeave} className="btn-2 self-stretch" onClick={()=>{handleCloseViewModal(); handleOpenProfile();}}>Profile</button>
             <button onMouseEnter={onEnter} onMouseLeave={onLeave} className="btn-2 self-stretch" onClick={()=>{handleCloseViewModal(); handleOpenPassword();}}>Settings</button>
+            <button onMouseEnter={onEnter} onMouseLeave={onLeave} className="btn-2 self-stretch" onClick={()=>{handleLogOut(); handleCloseViewModal();}}>Logout</button>
+
         </Modal>
     );
 }
