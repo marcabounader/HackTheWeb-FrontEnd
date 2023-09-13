7273a024d428e69d2f06fdcfab2c2108220d0bc6
marcabounader92 <marcabounader92@outlook.com>
2023-09-13 22:12:16 +0300

    Added get all labs and get statistics for admin

---
+++ hacktheweb-frontend/src/helpers/admin.helpers.js
@@ -0,0 +1,59 @@
+import axios from "axios";
+
+const baseUrl = 'http://localhost:8000';
+
+async function getAllLabs(token) {
+    try {
+      const res = await axios.get(`${baseUrl}/api/common/get-all-labs`, {headers: { Authorization: `Bearer ${token}` }});
+      if (res.status === 200) {
+        const data = res.data;
+        return { data };
+      }
+    } catch (error) {
+      const {
+        response: {
+          data: { message, errors },
+        },
+      } = error;
+  
+      if (errors) {
+        const errorMessages = Object.keys(errors).map((key) => {
+          const firstError = errors[key][0];
+          if (firstError) {
+            return firstError;
+          }
+        });
+        return { errorMessages };
+      }
+      return { message };
+    }
+  }
+
+  async function getAdminStatistics(token) {
+    try {
+      const res = await axios.get(`${baseUrl}/api/admin/statistics`, {headers: { Authorization: `Bearer ${token}` }});
+      if (res.status === 200) {
+        const data = res.data;
+        return { data };
+      }
+    } catch (error) {
+      const {
+        response: {
+          data: { message, errors },
+        },
+      } = error;
+  
+      if (errors) {
+        const errorMessages = Object.keys(errors).map((key) => {
+          const firstError = errors[key][0];
+          if (firstError) {
+            return firstError;
+          }
+        });
+        return { errorMessages };
+      }
+      return { message };
+    }
+  }
+
+  export {getAllLabs,getAdminStatistics};
