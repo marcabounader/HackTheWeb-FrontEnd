008c8feb648d0fd8449843781176fbe0db44d4a7
marcabounader92 <marcabounader92@outlook.com>
2023-09-13 21:34:17 +0300

    Removed completed_labs

--- hacktheweb-frontend/src/components/Content/CompletedLabs.jsx
+++ hacktheweb-frontend/src/components/Content/CompletedLabs.jsx
@@ -3,11 +3,10 @@ import LabCard from "../Cards/LabCard";
 
 const CompletedLabs = () => {
     const labs = useSelector((state) => state.labs.labs);
-    const completed_labs = useSelector((state) => state.labs.completedLabs);
     return ( 
         <>
         <h1 className=" text-start w-full">Completed Labs</h1>
-            {completed_labs && completed_labs.length > 0 ? (
+            {labs && labs.length > 0 ? (
                 labs
                 .filter((lab) => lab.isComplete)
                 .map((lab, index) => <LabCard lab={lab} key={index} />)
