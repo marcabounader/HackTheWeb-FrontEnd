8569e226dc945954ab382782b88ac9d4d82b1c0c
marcabounader92 <marcabounader92@outlook.com>
2023-09-13 17:51:43 +0300

    Added chatbot config prop

--- hacktheweb-frontend/src/components/Modals/ChatBotModal.jsx
+++ hacktheweb-frontend/src/components/Modals/ChatBotModal.jsx
@@ -13,6 +13,7 @@ const ChatBotModal = ({isOpen,handleCloseViewModal}) => {
         className="z-30 bg-[#0D1115] w-[90%] h-[90%] transition-opacity fixed top-1/2 left-1/2 transform  -translate-x-1/2 -translate-y-1/2 dark:border flex flex-col items-center gap-3 shadow-lg py-[10px] px-[20px]"
         overlayClassName="fixed top-0 z-10 left-0 w-[100vw] h-full backdrop-blur-xl drop-shadow-lg"
         >       
+        <Chatbot config={config}></Chatbot>
         </Modal>
         );
 }
