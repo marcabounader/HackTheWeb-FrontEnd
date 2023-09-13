8d21ca17d7f1f73c987025cf79f6cc14ab5c2692
marcabounader92 <marcabounader92@outlook.com>
2023-09-13 22:58:44 +0300

    Added get lab cat helper

--- hacktheweb-frontend/src/helpers/admin.helpers.js
+++ hacktheweb-frontend/src/helpers/admin.helpers.js
@@ -81,6 +81,32 @@ async function getAllLabs(token) {
       return { message };
     }
   }
+  async function getLabCategory(token) {
+    try {
+      const res = await axios.get(`${baseUrl}/api/admin/get-lab-categories`, {headers: { Authorization: `Bearer ${token}` }});
+      if (res.status === 200) {
+        const data = res.data;
+        return { data };
+      }
+    } catch (error) {
+      const {
+        response: {
+          data: { message, errors },
+        },
+      } = error;
+  
+      if (errors) {
+        const errorMessages = Object.keys(errors).map((key) => {
+          const firstError = errors[key][0];
+          if (firstError) {
+            return firstError;
+          }
+        });
+        return { errorMessages };
+      }
+      return { message };
+    }
+  }
   async function addLab(token,lab) {
     try {
       const res = await axios.post(`${baseUrl}/api/admin/add-lab`,lab, {headers: { Authorization: `Bearer ${token}` }});
@@ -107,4 +133,4 @@ async function getAllLabs(token) {
       return { message };
     }
   }
-  export {getAllLabs,getAdminStatistics,addLab};
+  export {getAllLabs,getAdminStatistics,addLab,getLabCategory,getLabDifficulty};
