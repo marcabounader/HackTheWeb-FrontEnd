c75ee7916ab6ace11c65e26a2cf92ed8607e882a
marcabounader92 <marcabounader92@outlook.com>
2023-09-13 19:51:25 +0300

    Added chatbot and modal css

--- hacktheweb-frontend/src/components/Modals/ChatBot/ChatBotModal.jsx
+++ hacktheweb-frontend/src/components/Modals/ChatBot/ChatBotModal.jsx
@@ -1,5 +1,6 @@
 import Modal from 'react-modal';
-import Chatbot from 'react-chatbot-kit'
+import { Chatbot } from 'react-chatbot-kit';
+import 'react-chatbot-kit/build/main.css';
 import ActionProvider from './ActionProvider';
 import MessageParser from './MessageParser';
 import config from './config';
@@ -10,10 +11,10 @@ const ChatBotModal = ({isOpen,handleCloseViewModal}) => {
         <Modal
         isOpen={isOpen}
         onRequestClose={handleCloseViewModal}
-        className="z-30 transition-opacity bg-bg-main fixed top-1/2 left-1/2 transform  -translate-x-1/2 -translate-y-1/2 dark:border dark:bg-slate-900 dark:text-slate-200 signIn-container flex flex-col items-center gap-5 rounded-2xl shadow-lg"
+        className="z-30 transition-opacity bg-bg-main fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 dark:border rounded-2xl shadow-lg"
         overlayClassName="fixed top-0 z-10 left-0 w-[100vw] h-full backdrop-blur-xl drop-shadow-lg"
-        >       
-        <Chatbot config={config} actionProvider={ActionProvider} messageParser={MessageParser}/>
+        >     
+            <Chatbot config={config} actionProvider={ActionProvider} messageParser={MessageParser}/>
         </Modal>
         );
 }
