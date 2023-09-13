e69abc16412b7dddef1b0fd920bd2d3466218df8
marcabounader92 <marcabounader92@outlook.com>
2023-09-13 22:49:19 +0300

    added plus icon for lab add

--- hacktheweb-frontend/src/components/Content/Labs.jsx
+++ hacktheweb-frontend/src/components/Content/Labs.jsx
@@ -1,14 +1,28 @@
-import { useEffect, useState } from "react";
-import { getLabs } from "../../helpers/user.helpers";
-import { useNavigate } from "react-router-dom";
 import LabCard from "../Cards/LabCard";
 import { useSelector } from "react-redux";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlusSquare } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+import AddLabModal from "../Modals/AddLabModal";
 
 const Labs = () => {
     const labs = useSelector((state) => state.labs.labs);
+    const user = useSelector((state) => state.user);
+    const [showLabAdd,setShowLabAdd] = useState(false);
+    const handleOpenLab = () => setShowLabAdd(true);
+    const handleCloseLab = () =>setShowLabAdd(false);
+    const { type_id} = user;    
     return ( 
         <>
+        <AddLabModal isOpen={showLabAdd} handleCloseViewModal={handleCloseLab}></AddLabModal>
+        {type_id=="3" ?
         <h1 className=" text-start w-full">Labs</h1>
+        :
+        <div className="flex flex-row basis-full justify-between items-center">
+            <h1 className=" text-start ">Labs</h1>
+            <FontAwesomeIcon onClick={handleOpenLab} icon={faPlusSquare} className="text-color-secondary w-[40px] h-[40px]" ></FontAwesomeIcon>
+        </div>
+        }
             {labs && labs.length > 0 ? (
                 labs
                 .map((lab, index) => <LabCard lab={lab} key={index} />)
