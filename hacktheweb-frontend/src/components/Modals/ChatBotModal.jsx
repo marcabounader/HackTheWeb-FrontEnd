cff844a293c64eddd3a6b033b050787262061ecc
marcabounader92 <marcabounader92@outlook.com>
2023-09-13 17:50:11 +0300

    Added modal css

--- hacktheweb-frontend/src/components/Modals/ChatBotModal.jsx
+++ hacktheweb-frontend/src/components/Modals/ChatBotModal.jsx
@@ -6,7 +6,7 @@ const ChatBotModal = () => {
         <Modal
         isOpen={isOpen}
         onRequestClose={handleCloseViewModal}
-
+        className="z-30 bg-[#0D1115] w-[90%] h-[90%] transition-opacity fixed top-1/2 left-1/2 transform  -translate-x-1/2 -translate-y-1/2 dark:border flex flex-col items-center gap-3 shadow-lg py-[10px] px-[20px]"
         >       
         </Modal>
         );
