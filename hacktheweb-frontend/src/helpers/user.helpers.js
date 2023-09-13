4ca83d75bb093fbb51c3974bb463ccd588d7dfbe
marcabounader92 <marcabounader92@outlook.com>
2023-09-13 22:13:28 +0300

    Removed get all labs from user helpers

--- hacktheweb-frontend/src/helpers/user.helpers.js
+++ hacktheweb-frontend/src/helpers/user.helpers.js
@@ -138,32 +138,7 @@ async function getLabs(token) {
   }
 }
 
-async function getAllLabs(token) {
-  try {
-    const res = await axios.get(`${baseUrl}/api/common/get-all-labs`, {headers: { Authorization: `Bearer ${token}` }});
-    if (res.status === 200) {
-      const data = res.data;
-      return { data };
-    }
-  } catch (error) {
-    const {
-      response: {
-        data: { message, errors },
-      },
-    } = error;
 
-    if (errors) {
-      const errorMessages = Object.keys(errors).map((key) => {
-        const firstError = errors[key][0];
-        if (firstError) {
-          return firstError;
-        }
-      });
-      return { errorMessages };
-    }
-    return { message };
-  }
-}
 
 async function getActiveLabs(token) {
   try {
