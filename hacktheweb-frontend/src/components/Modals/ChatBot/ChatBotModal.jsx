f46258d38a5a84dfa98c2035fa4d9f10815e8ffb
marcabounader92 <marcabounader92@outlook.com>
2023-09-13 19:16:20 +0300

    Added chatbo component with props

---
+++ hacktheweb-frontend/src/components/Modals/ChatBot/ChatBotModal.jsx
@@ -0,0 +1,21 @@
+import Modal from 'react-modal';
+import Chatbot from 'react-chatbot-kit'
+import ActionProvider from './ActionProvider';
+import MessageParser from './MessageParser';
+import config from './config';
+Modal.setAppElement('#root');
+
+const ChatBotModal = ({isOpen,handleCloseViewModal}) => {
+    return ( 
+        <Modal
+        isOpen={isOpen}
+        onRequestClose={handleCloseViewModal}
+        className="z-30 transition-opacity bg-bg-main fixed top-1/2 left-1/2 transform  -translate-x-1/2 -translate-y-1/2 dark:border dark:bg-slate-900 dark:text-slate-200 signIn-container flex flex-col items-center gap-5 rounded-2xl shadow-lg"
+        overlayClassName="fixed top-0 z-10 left-0 w-[100vw] h-full backdrop-blur-xl drop-shadow-lg"
+        >       
+        <Chatbot config={config} actionProvider={ActionProvider} messageParser={MessageParser}/>
+        </Modal>
+        );
+}
+ 
+export default ChatBotModal;
