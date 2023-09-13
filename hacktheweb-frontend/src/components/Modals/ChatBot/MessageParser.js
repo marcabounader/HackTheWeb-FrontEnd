7e5da16be8aea18b5c9bb3b878ea0d81f21594e7
marcabounader92 <marcabounader92@outlook.com>
2023-09-13 19:50:41 +0300

    Added hello greeting

--- hacktheweb-frontend/src/components/Modals/ChatBot/MessageParser.js
+++ hacktheweb-frontend/src/components/Modals/ChatBot/MessageParser.js
@@ -1,17 +1,21 @@
-import ActionProvider from "./ActionProvider";
-
-class MessageParser {
-    constructor(actionProvider) {
-      this.actionProvider = ActionProvider;
-    }
+import React from "react";
+const MessageParser = ({ children, actions }) => {
+    const parse = (message) => {
+        if (message.includes('hello')) {
+            actions.handleHello();
+        }    
+    };
   
-    parse(message) {
-      const lowerCaseMessage = message.toLowerCase()
-      
-      if (lowerCaseMessage.includes("hello")) {
-        this.actionProvider.greet()
-      }
-    }
-  }
+    return (
+      <div>
+        {React.Children.map(children, (child) => {
+          return React.cloneElement(child, {
+            parse: parse,
+            actions,
+          });
+        })}
+      </div>
+    );
+  };
   
-  export default MessageParser
+  export default MessageParser;
