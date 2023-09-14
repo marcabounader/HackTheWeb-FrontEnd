413c0a869e2de457a54cfb267ed9d104ba8ae81b
marcabounader92 <marcabounader92@outlook.com>
2023-09-15 00:48:50 +0300

    Sorted attributes

--- hacktheweb-frontend/src/components/Content/AdminActiveLabs.jsx
+++ hacktheweb-frontend/src/components/Content/AdminActiveLabs.jsx
@@ -6,13 +6,14 @@ const AdminActiveLabs = ({}) => {
 
     return ( 
         <>
-        <h1 className="text-start self-stretch w-full">Top Ten Hackers</h1>
+        <h1 className="text-start self-stretch w-full">User Active Labs</h1>
         <div className="flex flex-start gap-[10px] h-[64px] w-full items-center bg-black shadow-md">
-            <div className="basis-1/5  text-center uppercase">Rank</div>
-            <div className="basis-1/5 uppercase text-center">Name</div>
-            <div className="basis-1/5  text-center uppercase">Rewards</div>
-            <div className="basis-1/5  text-center uppercase">Completed Labs</div>
-            <div className="basis-1/5  text-center uppercase">Badges</div>
+            <div className="basis-1/5  text-center uppercase">Name</div>
+            <div className="basis-1/5 uppercase text-center">Email</div>
+            <div className="basis-1/5  text-center uppercase">Docker Name</div>
+            <div className="basis-1/5  text-center uppercase">Flag</div>
+            <div className="basis-1/5  text-center uppercase">Port</div>
+            <div className="basis-1/5  text-center uppercase">Launch Time</div>
         </div>
         {active_labs && active_labs.length > 0 ? (
             active_labs
