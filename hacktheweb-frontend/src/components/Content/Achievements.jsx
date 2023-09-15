7d9231c733f0da3c0e14da98f7627644673e6c51
marcabounader92 <marcabounader92@outlook.com>
2023-09-15 08:11:37 +0300

    Added badges header

--- hacktheweb-frontend/src/components/Content/Achievements.jsx
+++ hacktheweb-frontend/src/components/Content/Achievements.jsx
@@ -3,10 +3,15 @@ import BadgeCard from "../Cards/BadgeCard";
 
 const Achievements = () => {
     const badges = useSelector((state) => state.labs.badges);
-
+    const user = useSelector((state) => state.user);
+    const { type_id } = user;
     return ( 
         <>
+        { type_id=="3" ?
         <h1 className="text-start w-full">Achievements</h1>
+        :
+        <h1 className="text-start w-full">Badges</h1>
+        }
         {badges && badges.length > 0 ? (
         badges.map((badge, index) => <BadgeCard badge={badge} key={index} />)
         ) : (
