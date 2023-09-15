5f815e320d0782b35d2ed07992dd230ffd35daf9
marcabounader92 <marcabounader92@outlook.com>
2023-09-15 10:56:51 +0300

    Fixed badge lab width, height and img centering

--- hacktheweb-frontend/src/components/Cards/BadgeCard/index.jsx
+++ hacktheweb-frontend/src/components/Cards/BadgeCard/index.jsx
@@ -9,14 +9,19 @@ const BadgeCard = ({type_id,badge,token}) => {
         <>
         <AddBadgeModal badge={badge} isOpen={showModifyBadge} handleCloseViewModal={handleCloseModifyBadge} token={token}/>
         { type_id=="3" ?
-        <div className="flex w=[277px] h=[370px] p-[10px] flex-col items-center gap-[10px] bg-bg-card rounded-xl shadow-lg">
+        <div className="flex w-[277px] h-[370px] p-[10px] flex-col items-center gap-[10px] bg-bg-card rounded-xl shadow-lg">
             <h4>{badge.name}</h4>
+            <div className='flex flex-row justify-center flex-grow items-center'>
             <img src={badge.icon_url} alt={`${badge.name} icon`} className='w-[139px] h-[137px]'/>
+            </div>        
         </div>
         :
-        <div className="flex w=[277px] h=[370px] p-[10px] flex-col items-center gap-[10px] bg-bg-card rounded-xl shadow-lg cursor-pointer" onClick={handleOpenModifyBadge}>
+        <div className="flex w-[277px] h-[370px] p-[10px] flex-col items-center gap-[10px] bg-bg-card rounded-xl shadow-lg cursor-pointer" onClick={handleOpenModifyBadge}>
             <h4>{badge.name}</h4>
+            <div className='flex flex-row justify-center flex-grow items-center'>
             <img src={badge.icon_url} alt={`${badge.name} icon`} className='w-[139px] h-[137px]'/>
+            <div>{badge.</div>
+            </div>
         </div>
         }
         </>
