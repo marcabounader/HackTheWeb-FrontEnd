deb6646a65d739bb2a05f3f03934a1e0d9d2fcc5
marcabounader92 <marcabounader92@outlook.com>
2023-09-15 09:24:23 +0300

    Added modify badge handle

--- hacktheweb-frontend/src/components/Cards/BadgeCard/index.jsx
+++ hacktheweb-frontend/src/components/Cards/BadgeCard/index.jsx
@@ -1,6 +1,8 @@
 import './BadgeCard.css';
 const BadgeCard = ({type_id,badge}) => {
-        
+    const [showModifyBadge,setShowModifyBadge]=useState(false);
+    const handleCloseModifyBadge = () => setShowModifyBadge(false);
+    const handleOpenModifyBadge = () => setShowModifyBadge(true);
     return ( 
         <>
         { type_id=="3" ?
@@ -9,7 +11,7 @@ const BadgeCard = ({type_id,badge}) => {
             <img src={badge.icon_url} alt={`${badge.name} icon`} className='w-[139px] h-[137px]'/>
         </div>
         :
-        <div className="flex w=[277px] h=[370px] p-[10px] flex-col items-center gap-[10px] bg-bg-card rounded-xl shadow-lg cursor-pointer">
+        <div className="flex w=[277px] h=[370px] p-[10px] flex-col items-center gap-[10px] bg-bg-card rounded-xl shadow-lg cursor-pointer" onClick={handleOpen}>
             <h4>{badge.name}</h4>
             <img src={badge.icon_url} alt={`${badge.name} icon`} className='w-[139px] h-[137px]'/>
         </div>
