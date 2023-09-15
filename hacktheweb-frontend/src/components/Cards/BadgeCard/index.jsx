a3f48c36a61ad14adcc841c433fd01350794a2cd
marcabounader92 <marcabounader92@outlook.com>
2023-09-15 09:22:30 +0300

    Added badge condition for admin

--- hacktheweb-frontend/src/components/Cards/BadgeCard/index.jsx
+++ hacktheweb-frontend/src/components/Cards/BadgeCard/index.jsx
@@ -1,12 +1,20 @@
 import './BadgeCard.css';
-const BadgeCard = ({badge}) => {
+const BadgeCard = ({type_id,badge}) => {
         
     return ( 
+        <>
+        { type_id=="3" ?
         <div className="flex w=[277px] h=[370px] p-[10px] flex-col items-center gap-[10px] bg-bg-card rounded-xl shadow-lg">
             <h4>{badge.name}</h4>
             <img src={badge.icon_url} alt={`${badge.name} icon`} className='w-[139px] h-[137px]'/>
-
         </div>
+        :
+        <div className="flex w=[277px] h=[370px] p-[10px] flex-col items-center gap-[10px] bg-bg-card rounded-xl shadow-lg cursor-pointer">
+            <h4>{badge.name}</h4>
+            <img src={badge.icon_url} alt={`${badge.name} icon`} className='w-[139px] h-[137px]'/>
+        </div>
+        }
+        </>
     );
 }
  
