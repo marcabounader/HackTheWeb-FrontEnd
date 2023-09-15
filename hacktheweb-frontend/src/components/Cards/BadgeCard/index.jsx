226f867728e047bea49ae4c99e17166def1cad57
marcabounader92 <marcabounader92@outlook.com>
2023-09-15 09:26:53 +0300

    Added badge modal with props for modify

--- hacktheweb-frontend/src/components/Cards/BadgeCard/index.jsx
+++ hacktheweb-frontend/src/components/Cards/BadgeCard/index.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import './BadgeCard.css';
-const BadgeCard = ({type_id,badge}) => {
+import AddBadgeModal from '../../Modals/AddBadgeModal';
+const BadgeCard = ({type_id,badge,token}) => {
     const [showModifyBadge,setShowModifyBadge]=useState(false);
     const handleCloseModifyBadge = () => setShowModifyBadge(false);
     const handleOpenModifyBadge = () => setShowModifyBadge(true);
     return ( 
         <>
+        <AddBadgeModal badge={badge} isOpen={showModifyBadge} handleCloseViewModal={handleCloseModifyBadge} token={token}/>
         { type_id=="3" ?
         <div className="flex w=[277px] h=[370px] p-[10px] flex-col items-center gap-[10px] bg-bg-card rounded-xl shadow-lg">
             <h4>{badge.name}</h4>
