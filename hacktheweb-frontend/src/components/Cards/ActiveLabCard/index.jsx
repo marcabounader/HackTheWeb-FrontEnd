55c377715a86ead1c5fbf1fcf4c683ed58970c51
marcabounader92 <marcabounader92@outlook.com>
2023-09-15 12:35:58 +0300

    Added handle stop lab

--- hacktheweb-frontend/src/components/Cards/ActiveLabCard/index.jsx
+++ hacktheweb-frontend/src/components/Cards/ActiveLabCard/index.jsx
@@ -1,15 +1,24 @@
-import { useState } from "react";
-import LabModal from "../../Modals/LabModal";
 import { useSelector } from "react-redux";
 import { useDispatch } from 'react-redux';
 import { useNavigate } from "react-router-dom";
+import { stopLab } from "../../../helpers/admin.helpers";
+import { removeActiveLab, setLabInactive } from "../../../slices/labSlice";
 const ActiveLabCard = ({lab}) => {
     const dispatch= useDispatch();
 
     const navigate =useNavigate();
     const user = useSelector((state) => state.user);
     const { token ,type_id} = user;
-
+    const handleStopLab = async () => {
+        const {data , errorMessages, message} = await stopLab(token,lab.project_name);
+        console.log(data);
+        if(message && message=="Unauthenticated."){
+          navigate("/");
+        } else if (data && data.message) {
+            dispatch(removeActiveLab(lab.id));
+            dispatch(setLabInactive(lab.id));
+        }
+    }
     return ( 
         <div className="flex flex-start gap-[10px] h-[64px] w-full items-center bg-black shadow-md">
             <div className="basis-1/5 capitalize text-center">{lab.user_info.name}</div>
