de8a86339d9858590eeac9e96cbe6197121a5781
marcabounader92 <marcabounader92@outlook.com>
2023-09-15 09:02:38 +0300

    Added handle action for modify/add

--- hacktheweb-frontend/src/components/Modals/AddBadgeModal.jsx
+++ hacktheweb-frontend/src/components/Modals/AddBadgeModal.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import Modal from 'react-modal';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { addBadge } from '../../helpers/admin.helpers';
+import { setBadges } from '../../slices/labSlice';
 
 const AddBadgeModal = ({token,isOpen,handleCloseViewModal}) => {
     const initial_state = {
@@ -21,15 +23,55 @@ const AddBadgeModal = ({token,isOpen,handleCloseViewModal}) => {
         label: category.category,
       }));
       
-      useEffect(() => {
+    useEffect(() => {
         setInputState(initial_state);
         setErrors('');
         setSelectedImageName('');
     }, [isOpen]);
+
     function onChange(e) {
         const { value, name } = e.target;
           setInputState((prev) => ({ ...prev, [name]: value }));
-      }
+    }
+
+    const handleAction = async () => {
+        setErrors('');
+        if (lab) {
+            const modifiedInput = {};
+
+            for (const key in inputState) {
+                if (inputState[key] !== initial_state[key]) {
+                  modifiedInput[key] = inputState[key];
+                }
+              }
+              if (Object.keys(modifiedInput).length === 0) {
+                console.log("No changes to submit.");
+                return;
+              }
+          const { data, message, errorMessages } = await updateLab(token, lab.id, modifiedInput);
+          if (errorMessages) {
+            setErrors(errorMessages[0]);
+          } else if (message) {
+            setErrors(message);
+          } else if (message && message === "Unauthenticated.") {
+            navigate("/");
+          } else if (data && data.lab) {
+            dispatch(modifyLab(data.lab));
+          }
+        } else {
+          const { data, message, errorMessages } = await addBadge(token, inputState);
+          if (errorMessages) {
+            setErrors(errorMessages[0]);
+          } else if (message) {
+            setErrors(message);
+          } else if (message && message === "Unauthenticated.") {
+            navigate("/");
+          } else if (data && data.badge) {
+            dispatch(setBadges([...badges, data.badge]));
+            handleCloseViewModal();
+          }
+    }
+}
     return ( 
         <Modal
         isOpen={isOpen}
