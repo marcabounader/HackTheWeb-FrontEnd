e0d24385a8dd4006f5af3796d7c515774843845d
marcabounader92 <marcabounader92@outlook.com>
2023-09-15 09:24:44 +0300

    Added modify onclick

--- hacktheweb-frontend/src/components/Cards/BadgeCard/index.jsx
+++ hacktheweb-frontend/src/components/Cards/BadgeCard/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './BadgeCard.css';
 const BadgeCard = ({type_id,badge}) => {
     const [showModifyBadge,setShowModifyBadge]=useState(false);
@@ -11,7 +12,7 @@ const BadgeCard = ({type_id,badge}) => {
             <img src={badge.icon_url} alt={`${badge.name} icon`} className='w-[139px] h-[137px]'/>
         </div>
         :
-        <div className="flex w=[277px] h=[370px] p-[10px] flex-col items-center gap-[10px] bg-bg-card rounded-xl shadow-lg cursor-pointer" onClick={handleOpen}>
+        <div className="flex w=[277px] h=[370px] p-[10px] flex-col items-center gap-[10px] bg-bg-card rounded-xl shadow-lg cursor-pointer" onClick={handleOpenModifyBadge}>
             <h4>{badge.name}</h4>
             <img src={badge.icon_url} alt={`${badge.name} icon`} className='w-[139px] h-[137px]'/>
         </div>
