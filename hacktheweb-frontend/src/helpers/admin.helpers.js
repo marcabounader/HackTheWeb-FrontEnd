4592f0589f91037b448d090aed30638d311d42c0
marcabounader92 <marcabounader92@outlook.com>
2023-09-15 09:11:22 +0300

    Added updatebadge helper

--- hacktheweb-frontend/src/helpers/admin.helpers.js
+++ hacktheweb-frontend/src/helpers/admin.helpers.js
@@ -263,4 +263,30 @@ async function getAllLabs(token) {
       return { message };
     }
   }
-  export {getAllLabs,getAdminStatistics,addBadge,getActiveLabs,getBadges,getBadgeCategories,addLab,updateLab,getLabCategory,getLabDifficulty};
+  async function updatedBadge(token,badge_id,badge) {
+    try {
+      const res = await axios.put(`${baseUrl}/api/admin/modify-badge/${badge_id}`,badge, {headers: { Authorization: `Bearer ${token}` }});
+      if (res.status === 200) {
+        const data = res.data;
+        return { data };
+      }
+    } catch (error) {
+      const {
+        response: {
+          data: { message, errors },
+        },
+      } = error;
+  
+      if (errors) {
+        const errorMessages = Object.keys(errors).map((key) => {
+          const firstError = errors[key][0];
+          if (firstError) {
+            return firstError;
+          }
+        });
+        return { errorMessages };
+      }
+      return { message };
+    }
+  }
+  export {getAllLabs,getAdminStatistics,addBadge,updatedBadge,getActiveLabs,getBadges,getBadgeCategories,addLab,updateLab,getLabCategory,getLabDifficulty};
