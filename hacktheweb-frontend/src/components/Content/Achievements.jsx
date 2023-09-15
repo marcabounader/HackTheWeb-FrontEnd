0251e1aa199a3e9a5a4efdf7f70df2ecacb12cac
marcabounader92 <marcabounader92@outlook.com>
2023-09-15 08:15:35 +0300

    Added show badge state handler

--- hacktheweb-frontend/src/components/Content/Achievements.jsx
+++ hacktheweb-frontend/src/components/Content/Achievements.jsx
@@ -2,11 +2,15 @@ import { useSelector } from "react-redux";
 import BadgeCard from "../Cards/BadgeCard";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlusSquare } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 const Achievements = () => {
     const badges = useSelector((state) => state.labs.badges);
     const user = useSelector((state) => state.user);
     const { type_id } = user;
+    const [showAddBadge,setShowAddBadge]=useState(false);
+    handleCloseAddBadge = () => setShowAddBadge(false);
+    handleOpenAddBadge = () => setShowAddBadge(true);
     return ( 
         <>
         { type_id=="3" ?
@@ -14,8 +18,7 @@ const Achievements = () => {
         :
         <div className="flex flex-row basis-full justify-between items-center">
         <h1 className="text-start">Badges</h1>
-        <FontAwesomeIcon icon={faPlusSquare} className="text-color-secondary w-[40px] h-[40px]" ></FontAwesomeIcon>
-
+        <FontAwesomeIcon onClick={handleOpenAddBadge} icon={faPlusSquare} className="text-color-secondary w-[40px] h-[40px]" ></FontAwesomeIcon>
         </div>
 
         }
