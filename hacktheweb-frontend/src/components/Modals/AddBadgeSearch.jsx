101b29759c4f69dc9118d76a5e26bc15c07e4bf9
marcabounader92 <marcabounader92@outlook.com>
2023-09-15 10:33:48 +0300

    Added handle lab search for modal

--- hacktheweb-frontend/src/components/Modals/AddBadgeSearch.jsx
+++ hacktheweb-frontend/src/components/Modals/AddBadgeSearch.jsx
@@ -1,11 +1,24 @@
 import Modal from 'react-modal';
 
-const AddBadgeSearch = () => {
+const AddBadgeSearch = ({setSelectedLabId,labs}) => {
+    const handleLabSearch = (e) => {
+        const searchInput = e.target.value;
+        if(searchInput!='')
+        {
+            const filtered = labs.filter((lab) =>
+            lab.name.toLowerCase().includes(searchInput.toLowerCase())
+          );
+          setFilteredLabs(filtered);
+        }
+      };
+      const handleLabSelect = (lab_id) => {
+        setSelectedLabId(lab_id);
+      };
     return ( 
         <Modal
         isOpen={isOpen}
         onRequestClose={handleCloseViewModal}
-        className="z-30 transition-opacity bg-bg-main fixed top-1/2 left-1/2 transform  -translate-x-1/2 -translate-y-1/2 dark:border dark:bg-slate-900 dark:text-slate-200 signIn-container flex flex-col items-center gap-5 rounded-2xl shadow-lg"
+        className="z-35 transition-opacity bg-bg-main fixed top-1/2 left-1/2 transform  -translate-x-1/2 -translate-y-1/2 dark:border dark:bg-slate-900 dark:text-slate-200 signIn-container flex flex-col items-center gap-5 rounded-2xl shadow-lg"
         overlayClassName="fixed top-0 z-10 left-0 w-[100vw] h-full backdrop-blur-xl drop-shadow-lg"
       >
         <h4 className="p-4">Search for Lab</h4>
