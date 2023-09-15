5fbd190a5678bbea9cf280b6c6dded9947638229
marcabounader92 <marcabounader92@outlook.com>
2023-09-15 09:19:38 +0300

    Added buttons

--- hacktheweb-frontend/src/components/Modals/AddBadgeModal.jsx
+++ hacktheweb-frontend/src/components/Modals/AddBadgeModal.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom';
 import { addBadge, updatedBadge } from '../../helpers/admin.helpers';
 import { modifyBadge, setBadges } from '../../slices/labSlice';
 import CustomInput from '../Inputs/CustomInput';
-import Select from 'react-select/dist/declarations/src/Select';
+import Select from 'react-select';
 
 const AddBadgeModal = ({badge,token,isOpen,handleCloseViewModal}) => {
     const initial_state = {
@@ -98,6 +98,9 @@ const AddBadgeModal = ({badge,token,isOpen,handleCloseViewModal}) => {
         className="z-30 transition-opacity bg-bg-main fixed top-1/2 left-1/2 transform  -translate-x-1/2 -translate-y-1/2 dark:border dark:bg-slate-900 dark:text-slate-200 signIn-container flex flex-col items-center gap-5 rounded-2xl shadow-lg"
         overlayClassName="fixed top-0 z-10 left-0 w-[100vw] h-full backdrop-blur-xl drop-shadow-lg"
       >
+        <h4 className="p-4">Add Badge</h4>
+        <div className="flex flex-row gap-5 p-6 pb-0">
+        <div className='flex flex-col gap-5 basis-[50%]'>
         <CustomInput
             label="Name"
             name="name"
@@ -114,7 +117,7 @@ const AddBadgeModal = ({badge,token,isOpen,handleCloseViewModal}) => {
             value={category_options.find(option => option.value === category_id)}
             onChange={(selectedOption) => setInputState((prev) => ({ ...prev, category_id: selectedOption.value }))}
             options={category_options} 
-            placeholder='Select a difficulty'
+            placeholder='Select a category'
             >
             </Select> 
         <div className='flex flex-col basis-full self-stretch justify-end'>
@@ -124,6 +127,18 @@ const AddBadgeModal = ({badge,token,isOpen,handleCloseViewModal}) => {
             <input type="file" name="icon" accept="image/*" onChange={fileHandler} style={{ display: 'none' }} />
             </label>
           </div>
+          </div>
+          </div>
+          <div className="error font-normal text-red-700 text-sm">{errors}</div>
+        <div className=" monster flex justify-between gap-3 w-full px-5 pb-5">
+          <button onClick={() => handleAction()} className="btn primary-btn">
+            {badge ? 'Modify' : 'Add'}
+          </button>
+
+          <button onClick={handleCloseViewModal} className="btn">
+            Cancel
+          </button>
+        </div>
       </Modal>
     );
 }
