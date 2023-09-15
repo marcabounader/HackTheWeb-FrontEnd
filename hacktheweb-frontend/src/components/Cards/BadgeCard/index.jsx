f8cda660913437fd24b89fee394fab4b906cda13
marcabounader92 <marcabounader92@outlook.com>
2023-09-15 11:05:04 +0300

    Added handle delete and button

--- hacktheweb-frontend/src/components/Cards/BadgeCard/index.jsx
+++ hacktheweb-frontend/src/components/Cards/BadgeCard/index.jsx
@@ -1,10 +1,23 @@
 import { useState } from 'react';
 import './BadgeCard.css';
 import AddBadgeModal from '../../Modals/AddBadgeModal';
-const BadgeCard = ({type_id,badge,token}) => {
+import { setBadges } from '../../../slices/labSlice';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+const BadgeCard = ({type_id,badges,badge,token}) => {
     const [showModifyBadge,setShowModifyBadge]=useState(false);
     const handleCloseModifyBadge = () => setShowModifyBadge(false);
     const handleOpenModifyBadge = () => setShowModifyBadge(true);
+    const navigate=useNavigate();
+    const dispatch = useDispatch();
+    const handleDelete = async () =>{
+        const {data , errorMessages, message} = await deleteBadge(token,badge.id);
+        if(message && message=="Unauthenticated."){
+          navigate("/");
+        } else if (data && data.message) {
+            dispatch(setBadges(badges.filter((old_badge) => old_badge.id !== badge.id)));
+        }
+    }
     return ( 
         <>
         <AddBadgeModal badge={badge} isOpen={showModifyBadge} handleCloseViewModal={handleCloseModifyBadge} token={token}/>
@@ -20,8 +33,8 @@ const BadgeCard = ({type_id,badge,token}) => {
             <h4>{badge.name}</h4>
             <div className='flex flex-row justify-center flex-grow items-center'>
             <img src={badge.icon_url} alt={`${badge.name} icon`} className='w-[139px] h-[137px]'/>
-            <div>{badge.</div>
             </div>
+            <button className='btn-2 secondary-btn self-end' onClick={handleDelete}>Delete</button>
         </div>
         }
         </>
