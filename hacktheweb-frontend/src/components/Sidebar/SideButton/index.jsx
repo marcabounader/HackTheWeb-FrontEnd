b3d8b0502d23ebaa0a069c106d16d1c7e300f134
marcabounader92 <marcabounader92@outlook.com>
2023-09-16 16:03:27 +0300

    Added gsp to sidebuttons

--- hacktheweb-frontend/src/components/Sidebar/SideButton/index.jsx
+++ hacktheweb-frontend/src/components/Sidebar/SideButton/index.jsx
@@ -1,8 +1,8 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-const SideButton = ({ icon, text, onClick,className, fill, icon_style=null}) => {
+const SideButton = ({onEnter,onLeave, icon, text, onClick,className, fill, icon_style=null}) => {
   return (
-    <div className={`self-stretch items-center flex gap-[10px] h-[60px] cursor-pointer ${className}`} onClick={onClick}>
+    <div className={`self-stretch items-center flex gap-[10px] h-[60px] cursor-pointer ${className}`} onClick={onClick} onMouseEnter={onEnter} onMouseLeave={onLeave}>
       {icon && (
         <div>
           <FontAwesomeIcon icon={icon} width="35px" height="35px" className={icon_style}/>
