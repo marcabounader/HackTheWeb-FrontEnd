37b4febb6056572462581904b208ff6a3a32d68b
marcabounader92 <marcabounader92@outlook.com>
2023-09-19 10:25:46 +0300

    Moved pagination and fetch labs into labs

--- hacktheweb-frontend/src/components/Content/Labs.jsx
+++ hacktheweb-frontend/src/components/Content/Labs.jsx
@@ -1,19 +1,52 @@
 import LabCard from "../Cards/LabCard";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlusSquare } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AddLabModal from "../Modals/AddLabModal";
 import Pagination from "@mui/material/Pagination";
 import Stack from "@mui/material/Stack";
+import { useNavigate } from "react-router-dom";
+import { getLabs } from "../../helpers/user.helpers";
+import { setLabs } from "../../slices/labSlice";
 
-const Labs = ({currentPage,totalPages,handlePageChange}) => {
+const Labs = ({labs_tab}) => {
+    const [currentPage,setCurrentPage]=useState(1);
+    const [totalPages,setTotalPages]=useState(1);
+    const [perPage,setPerPage]=useState(9);
+    const [totalLabs,setTotalLabs] = useState('');
+    const dispatch=useDispatch();
+    const navigate=useNavigate();
     const labs = useSelector((state) => state.labs.labs);
     const user = useSelector((state) => state.user);
     const [showLabAdd,setShowLabAdd] = useState(false);
     const handleOpenLab = () => setShowLabAdd(true);
     const handleCloseLab = () =>setShowLabAdd(false);
-    const { type_id,token} = user;    
+    const { type_id,token} = user;   
+    const fetchLabs = async () => {
+        try {
+          const { data, message, errorMessages } = await getLabs(token,currentPage);
+          if (message && message === "Unauthenticated.") {
+            navigate("/");
+          } else if (data && data.labs) {
+            dispatch(setLabs(data.labs.data));
+            setTotalPages(data.labs.last_page);
+            setPerPage(data.labs.per_page);
+            setTotalLabs(data.labs.total);
+          }
+        } catch (error) {
+          console.error("Error fetching labs:", error);
+        }
+      };
+      const handlePageChange = (event,page) => {
+        setCurrentPage(page);
+        fetchLabs();
+      };
+      useEffect(() => {
+        if (labs_tab) {  
+          fetchLabs();
+        }
+      }, [labs_tab]); 
     return ( 
         <>
         <AddLabModal isOpen={showLabAdd} token={token} handleCloseViewModal={handleCloseLab}></AddLabModal>
@@ -25,7 +58,7 @@ const Labs = ({currentPage,totalPages,handlePageChange}) => {
             <FontAwesomeIcon onClick={handleOpenLab} icon={faPlusSquare} className="text-color-secondary w-[40px] h-[40px]" ></FontAwesomeIcon>
         </div>
         }
-        <Stack className="basis-full flex flex-col items-center">
+            <Stack className="basis-full flex flex-col items-center">
                 <Pagination
                 count={totalPages}
                 page={currentPage}
