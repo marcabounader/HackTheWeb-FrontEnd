3047b8c207a4b37a0fce72d3657e61e9db09b994
marcabounader92 <marcabounader92@outlook.com>
2023-09-19 12:38:45 +0300

    Added handle restrict dispatches

--- hacktheweb-frontend/src/components/Cards/AdminUserCard/index.jsx
+++ hacktheweb-frontend/src/components/Cards/AdminUserCard/index.jsx
@@ -1,38 +1,25 @@
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { restrict } from "../../../helpers/admin.helpers";
+import { setUserRestricted, setUserUnrestricted } from "../../../slices/labSlice";
 
-const AdminUsercard = ({setUsers,user,token}) => {
-        const navigate=useNavigate();
-        const dispatch=useDispatch();
-        const handleRestrict = async () =>{
-            const {data,message,errorMessages} = await restrict(token,user.id);
-            if(message && message=="Unauthenticated."){
-            navigate("/");
-            } else if (data) {
-                if (data.message === "unrestricted") {
-                    setUsers((prev) => {
-                        const updatedUsers = prev.map((u) => {
-                            if (u.id === user.id) {
-                                return { ...u, is_restricted: 0 };
-                            }
-                            return u;
-                        });
-                        return updatedUsers;
-                    });
-                } else if (data.message === "restricted") {
-                    setUsers((prev) => {
-                        const updatedUsers = prev.map((u) => {
-                            if (u.id === user.id) {
-                                return { ...u, is_restricted: 1 };
-                            }
-                            return u;
-                        });
-                        return updatedUsers;
-                    });
-                }
+const AdminUsercard = ({token,user}) => {
+    const navigate=useNavigate();
+    const dispatch=useDispatch();
+
+    const handleRestrict = async () =>{
+        const {data,message,errorMessages} = await restrict(token,user.id);
+        if(message && message=="Unauthenticated."){
+        navigate("/");
+        } else if (data) {
+            if (data.message === "unrestricted") {
+                dispatch(setUserUnrestricted(user.id))
+
+            } else if (data.message === "restricted") {
+                dispatch(setUserRestricted(user.id))
             }
         }
+    }
     return ( 
         <div className="flex flex-start gap-[10px] h-[64px] w-full items-center bg-black shadow-md">
             <div className="basis-1/5 text-center">{user.rank}</div>
