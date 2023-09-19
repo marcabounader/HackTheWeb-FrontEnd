f12ebb8ac23aea93e7a332d4bed7a7617ced48f2
marcabounader92 <marcabounader92@outlook.com>
2023-09-19 16:01:42 +0300

    Added pagination handling and components

--- hacktheweb-frontend/src/components/Content/AdminActiveLabs.jsx
+++ hacktheweb-frontend/src/components/Content/AdminActiveLabs.jsx
@@ -1,12 +1,29 @@
 import { useSelector } from "react-redux";
 import ActiveLabCard from "../Cards/ActiveLabCard";
+import { Pagination, Stack } from "@mui/material";
+import { useEffect } from "react";
 
-const AdminActiveLabs = ({}) => {
+const AdminActiveLabs = ({setCurrentPage,fetchActiveLabs,totalPages,currentPage}) => {
     const active_labs = useSelector((state) => state.labs.activeLabs);
-
+    const handlePageChange = (event,page) => {
+      setCurrentPage(page);
+      fetchActiveLabs();
+    };
+    useEffect(() => {
+        setCurrentPage(1);
+        fetchActiveLabs();
+    }, []); 
     return ( 
         <>
         <h1 className="text-start self-stretch w-full">User Active Labs</h1>
+        <Stack className="basis-full flex flex-col items-center">
+                <Pagination
+                count={totalPages}
+                page={currentPage}
+                onChange={handlePageChange}
+                color="primary"
+                />
+        </Stack>
         <div className="flex flex-start gap-[10px] h-[64px] w-full items-center bg-black shadow-md">
             <div className="basis-1/5  text-center uppercase">Name</div>
             <div className="basis-1/5 uppercase text-center">Email</div>
