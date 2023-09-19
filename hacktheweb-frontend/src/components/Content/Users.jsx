c7a5d84348bf79d93b6dede932c866b9131dff00
marcabounader92 <marcabounader92@outlook.com>
2023-09-19 12:40:07 +0300

    Added users props for pagination with pagination components

--- hacktheweb-frontend/src/components/Content/Users.jsx
+++ hacktheweb-frontend/src/components/Content/Users.jsx
@@ -1,31 +1,41 @@
 import { useEffect, useState } from "react";
 import AdminUsercard from "../Cards/AdminUserCard";
-import { getUsers } from "../../helpers/admin.helpers";
+import { Pagination, Stack } from "@mui/material";
+import { useSelector } from "react-redux";
 
-const Users = ({token}) => {
-    const [users,setUsers] = useState('');
+
+const Users = ({token,fetchUsers,setCurrentPage,totalPages,currentPage}) => {
+    const users = useSelector((state) => state.labs.users);
+
+    const handlePageChange = (event,page) => {
+        setCurrentPage(page);
+        fetchUsers();
+    };
     useEffect(()=>{
-        const fetchUsers = async () =>{
-            const {data,message,errorMessages} = await getUsers(token);
-            setUsers(data.users);
-        };
         fetchUsers();
     },[])
+
     return ( 
         <>
-        <h1 className="text-start self-stretch w-full">Top Ten Hackers</h1>
+        <h1 className="text-start self-stretch w-full">Hackers</h1>
+        <Stack className="basis-full flex flex-col items-center">
+                <Pagination
+                count={totalPages}
+                page={currentPage}
+                onChange={handlePageChange}
+                color="primary"
+                />
+        </Stack>
         <div className="flex flex-start gap-[10px] h-[64px] w-full items-center bg-black shadow-md">
             <div className="basis-1/5  text-center uppercase">Rank</div>
             <div className="basis-1/5 uppercase text-center">Name</div>
             <div className="basis-1/5  text-center uppercase">Email</div>
             <div className="basis-1/5  text-center uppercase">Status</div>
             <div className="basis-1/5  text-center uppercase">Restrict</div>
-            {/* <div className="basis-1/5  text-center uppercase">Delete</div> */}
-
         </div>
         {users && users.length > 0 ? (
             users
-            .map((user, index) => <AdminUsercard setUsers={setUsers} user={user} token={token} key={index} />)
+            .map((user, index) => <AdminUsercard user={user} token={token} key={index} />)
         ) : (
           <p>No Users.</p>
         )}
