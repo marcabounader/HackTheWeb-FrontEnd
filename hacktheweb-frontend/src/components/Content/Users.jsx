31284a7593f66e3adabd4822aa9af462447ae0ec
marcabounader92 <marcabounader92@outlook.com>
2023-09-20 19:42:10 +0300

    Pagination to bottom page and header left align

--- hacktheweb-frontend/src/components/Content/Users.jsx
+++ hacktheweb-frontend/src/components/Content/Users.jsx
@@ -18,24 +18,28 @@ const Users = ({token,fetchUsers,setCurrentPage,totalPages,currentPage}) => {
     return ( 
         <>
         <h1 className="text-start self-stretch w-full">Hackers</h1>
-        <Stack className="basis-full flex flex-col items-center">
+        <div className="flex flex-start gap-[10px] h-[64px] w-full items-center bg-black shadow-md">
+            <div className="basis-1/5  text-left uppercase ml-2">Rank</div>
+            <div className="basis-1/5 uppercase text-left">Name</div>
+            <div className="basis-1/5  text-left uppercase">Email</div>
+            <div className="basis-1/5  text-left uppercase">Status</div>
+            <div className="basis-1/5  text-left uppercase">Restrict</div>
+        </div>
+        {users && users.length > 0 ? (
+            <>
+            <div className="flex flex-col w-full gap-[5px]">
+            {users
+            .map((user, index) => <AdminUsercard user={user} token={token} key={index} />)}
+            </div>
+            <Stack className="basis-full flex flex-col items-center">
                 <Pagination
                 count={totalPages}
                 page={currentPage}
                 onChange={handlePageChange}
                 color="primary"
-                />
-        </Stack>
-        <div className="flex flex-start gap-[10px] h-[64px] w-full items-center bg-black shadow-md">
-            <div className="basis-1/5  text-center uppercase">Rank</div>
-            <div className="basis-1/5 uppercase text-center">Name</div>
-            <div className="basis-1/5  text-center uppercase">Email</div>
-            <div className="basis-1/5  text-center uppercase">Status</div>
-            <div className="basis-1/5  text-center uppercase">Restrict</div>
-        </div>
-        {users && users.length > 0 ? (
-            users
-            .map((user, index) => <AdminUsercard user={user} token={token} key={index} />)
+            />
+            </Stack>
+            </>
         ) : (
           <p>No Users.</p>
         )}
