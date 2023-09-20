b60f59b6d7b6db53ac050c8394021b6d33def4f6
marcabounader92 <marcabounader92@outlook.com>
2023-09-20 19:41:51 +0300

    Table header left align

--- hacktheweb-frontend/src/components/Content/Leaderboard.jsx
+++ hacktheweb-frontend/src/components/Content/Leaderboard.jsx
@@ -25,18 +25,20 @@ const Leaderboard = () => {
         <>
         <h1 className="text-start self-stretch w-full">Top Ten Hackers</h1>
         <div className="flex flex-start gap-[10px] h-[64px] w-full items-center bg-black shadow-md">
-            <div className="basis-1/5  text-center uppercase">Rank</div>
-            <div className="basis-1/5 uppercase text-center">Name</div>
-            <div className="basis-1/5  text-center uppercase">Rewards</div>
-            <div className="basis-1/5  text-center uppercase">Completed Labs</div>
-            <div className="basis-1/5  text-center uppercase">Badges</div>
+            <div className="basis-1/5  text-left uppercase ml-2">Rank</div>
+            <div className="basis-1/5 uppercase text-left">Name</div>
+            <div className="basis-1/5  text-left uppercase">Rewards</div>
+            <div className="basis-1/5  text-left uppercase">Completed Labs</div>
+            <div className="basis-1/5  text-left uppercase">Badges</div>
         </div>
+        <div className="flex flex-col w-full gap-[5px]">
         {topTen && topTen.length > 0 ? (
           topTen
             .map((user_rank, index) => <UserCard user_rank={user_rank} key={index} />)
         ) : (
           <p>No Top Hackers.</p>
         )}
+        </div>
       </>
      );
 }
