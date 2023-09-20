d583e271852af8ac5bc6b5f32f1939baf2e28e0f
marcabounader92 <marcabounader92@outlook.com>
2023-09-20 19:43:04 +0300

    Modified title and primary button to right

--- hacktheweb-frontend/src/components/Modals/AddLabModal.jsx
+++ hacktheweb-frontend/src/components/Modals/AddLabModal.jsx
@@ -78,7 +78,7 @@ const AddLabModal = ({lab,token,isOpen,handleCloseViewModal}) => {
             setErrors(message);
           } else if (message && message === "Unauthenticated.") {
             navigate("/");
-          } else if (data && data.lab) {
+          } else if (data && data.lab) {        
             dispatch(setLabs([...labs, data.lab]));
             handleCloseViewModal();
           }
@@ -105,10 +105,11 @@ const AddLabModal = ({lab,token,isOpen,handleCloseViewModal}) => {
         <Modal
         isOpen={isOpen}
         onRequestClose={handleCloseViewModal}
-        className="z-30 transition-opacity bg-bg-main fixed top-1/2 left-1/2 transform  -translate-x-1/2 -translate-y-1/2 dark:border dark:bg-slate-900 dark:text-slate-200 signIn-container flex flex-col items-center gap-5 rounded-2xl shadow-lg"
+        className="z-30 transition-opacity bg-bg-main fixed top-1/2 left-1/2 transform  -translate-x-1/2 -translate-y-1/2 dark:border flex flex-col items-center gap-5 rounded-2xl shadow-lg"
         overlayClassName="fixed top-0 z-10 left-0 w-[100vw] h-full backdrop-blur-xl drop-shadow-lg"
       >
-        <h4 className="p-4">Add Lab</h4>
+      
+        <h4 className="p-4">{lab ? 'Modify' : 'Add'} Lab</h4>
         <div className="flex flex-row gap-5 p-6 pb-0">
         <div className='flex flex-col gap-5 basis-[50%]'>
         <CustomInput
@@ -142,7 +143,7 @@ const AddLabModal = ({lab,token,isOpen,handleCloseViewModal}) => {
             </p>
           )}          
           <label className="btn-2 secondary-btn self-start">
-            Upload File
+            Upload Icon
             <input type="file" name="icon" accept="image/*" onChange={fileHandler} style={{ display: 'none' }} />
           </label>
           </div>
@@ -189,12 +190,13 @@ const AddLabModal = ({lab,token,isOpen,handleCloseViewModal}) => {
 
         <div className="error font-normal text-red-700 text-sm">{errors}</div>
         <div className=" monster flex justify-between gap-3 w-full px-5 pb-5">
-          <button onClick={() => handleAction()} className="btn primary-btn">
-            {lab ? 'Modify' : 'Add'}
-          </button>
+
 
           <button onClick={handleCloseViewModal} className="btn">
             Cancel
+          </button>
+          <button onClick={() => handleAction()} className="btn primary-btn">
+            {lab ? 'Modify' : 'Add'}
           </button>
         </div>
       </Modal>
