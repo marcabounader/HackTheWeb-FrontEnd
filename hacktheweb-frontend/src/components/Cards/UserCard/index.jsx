adb547379204d498ae3ab1586bd9f99ed63e68c7
marcabounader92 <marcabounader92@outlook.com>
2023-09-20 19:40:36 +0300

    Table header left align

--- hacktheweb-frontend/src/components/Cards/UserCard/index.jsx
+++ hacktheweb-frontend/src/components/Cards/UserCard/index.jsx
@@ -2,12 +2,12 @@ import { useSelector } from "react-redux";
 
 const UserCard = ({user_rank}) => {
     return ( 
-        <div className="flex flex-start gap-[10px] h-[64px] w-full items-center bg-black shadow-md">
-            <div className="basis-1/5 text-center">{user_rank.rank}</div>
-            <div className="basis-1/5 flex-grow capitalize text-center">{user_rank.name}</div>
-            <div className="basis-1/5 flex-grow text-center">{user_rank.rewards}</div>
-            <div className="basis-1/5 flex-grow text-center">{user_rank.completed_labs_count}</div>
-            <div className="basis-1/5 flex-grow text-center">{user_rank.badges_count}</div>
+        <div className="flex flex-start gap-[10px] h-[64px] w-full items-center bg-black opacity-75 shadow-md">
+            <div className="basis-1/5 text-left ml-2">{user_rank.rank}</div>
+            <div className="basis-1/5 flex-grow capitalize text-left">{user_rank.name}</div>
+            <div className="basis-1/5 flex-grow text-left">{user_rank.rewards}</div>
+            <div className="basis-1/5 flex-grow text-left">{user_rank.completed_labs_count}</div>
+            <div className="basis-1/5 flex-grow text-left">{user_rank.badges_count}</div>
         </div>
     );
 }
