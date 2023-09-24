e81b880e71cfeb68c80b98b1b200adf34eb09697
marcabounader92 <marcabounader92@outlook.com>
2023-09-24 12:37:23 +0300

    Import hero cyber logo and added svg component

--- hacktheweb-frontend/src/pages/Landing/index.jsx
+++ hacktheweb-frontend/src/pages/Landing/index.jsx
@@ -4,6 +4,7 @@ import LoginForm from "../../components/Forms/LoginForm";
 import RegisterForm from "../../components/Forms/RegisterForm";
 import Circle from "../../components/Objects/circle";
 import "./landing.css"
+import CyberSecurityLogo from "../../assets/logos/logo-cyber-security.svg"; // Import the SVG
 
 const Landing = ({addCircleRef,onEnter,onLeave,isLoginModalOpen,handleOpenLoginModal, handleCloseLoginModal,isRegisterModalOpen,handleCloseRegisterModal,handleOpenRegisterModal,areCirclesVisible}) => {
     const circleRefs = useRef([]);
@@ -28,14 +29,22 @@ const Landing = ({addCircleRef,onEnter,onLeave,isLoginModalOpen,handleOpenLoginM
             <LoginForm isOpen={isLoginModalOpen} handleCloseViewModal={handleCloseLoginModal} handleOpenRegisterModal={handleOpenRegisterModal}/>
             <RegisterForm isOpen={isRegisterModalOpen} handleCloseViewModal={handleCloseRegisterModal} handleOpenLoginModal={handleOpenLoginModal} setMyEmail={setMyEmail} myEmail={myEmail}/>
             {circles}
-            <div className="flex flex-col justify-center items-center">
-                <h1>Where offense becomes the ultimate defense!</h1>
-                <p>Here is where you'll uncover the secrets to safeguarding digital fortresses in an ever-evolving digital landscape.</p>
+            <div className="flex flex-row px-[20px] self-stretch">
+                <div className="flex flex-col justify-center basis-3/6">
+                    <div className="flex flex-col justify-center items-center">
+                        <h1>Where offense becomes the ultimate defense!</h1>
+                        <p>Here is where you'll uncover the secrets to safeguarding digital fortresses in an ever-evolving digital landscape.</p>
+                    </div>
+                    <div className="my-5">
+                        <input type="text" placeholder="Email" className="" value={myEmail} onChange={onChange}/>
+                        <button className="btn primary-btn" onMouseEnter={onEnter} onMouseLeave={onLeave} onClick={handleOpenRegisterModal}>Start Hacking</button>
+                    </div>   
+                </div>
+                <div className="flex flex-col self-stretch justify-center items-center basis-3/6 ">
+                <img src={CyberSecurityLogo} alt="Cyber Security Logo"/>
+                </div>
             </div>
-            <div className="my-5">
-                <input type="text" placeholder="Email" className="" value={myEmail} onChange={onChange}/>
-                <button className="btn primary-btn" onMouseEnter={onEnter} onMouseLeave={onLeave} onClick={handleOpenRegisterModal}>Start Hacking</button>
-            </div>   
+
             <Footer/>
         </section>
     );
