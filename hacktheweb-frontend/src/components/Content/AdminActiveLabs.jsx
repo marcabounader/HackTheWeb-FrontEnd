ee29ecf8ead89cb3dc8a31060263802d8b80e3a8
marcabounader92 <marcabounader92@outlook.com>
2023-09-25 12:59:28 +0300

    Added active labs search labs and debounce

--- hacktheweb-frontend/src/components/Content/AdminActiveLabs.jsx
+++ hacktheweb-frontend/src/components/Content/AdminActiveLabs.jsx
@@ -1,21 +1,46 @@
 import { useSelector } from "react-redux";
 import ActiveLabCard from "../Cards/ActiveLabCard";
-import { Pagination, Stack, ThemeProvider } from "@mui/material";
-import { useEffect } from "react";
+import { Pagination, Stack, ThemeProvider, debounce } from "@mui/material";
+import { useEffect, useState } from "react";
 
-const AdminActiveLabs = ({theme, setCurrentPage,fetchActiveLabs,totalPages,currentPage}) => {
+const AdminActiveLabs = ({handleActiveSearch, searchedActive,setSearchedActive,theme, setCurrentPage,fetchActiveLabs,totalPages,currentPage}) => {
     const active_labs = useSelector((state) => state.labs.activeLabs);
+    const debouncedHandleSearch = debounce(handleActiveSearch, 300);
+    const [debouncedSearch, setDebouncedSearch] = useState("");
+    const handleSearchChange = (event) => {
+      const { value } = event.target;
+      setDebouncedSearch(value);
+      if (value !== "" && value !== " " && active_labs && active_labs.length>0) {
+          debouncedHandleSearch(value);
+      } else {
+          setSearchedActive([]);
+      }
+    };
     const handlePageChange = (event,page) => {
       setCurrentPage(page);
-      fetchActiveLabs();
+      if(searchedActive && searchedActive.length > 0){
+        handleActiveSearch(debouncedSearch)
+      } else {
+        fetchActiveLabs();
+      }
     };
     useEffect(() => {
         setCurrentPage(1);
         fetchActiveLabs();
+        setSearchedActive([]);
+        setDebouncedSearch('');
     }, []); 
     return ( 
         <>
         <h1 className="text-start self-stretch w-full">User Active Labs</h1>
+        <div className="w-full flex flex-row justify-center items-center">
+        <input
+                    type="search"
+                    placeholder="Search Active Labs"
+                    value={debouncedSearch}
+                    onChange={handleSearchChange}
+        />
+        </div>
         <div className="flex flex-start gap-[10px] h-[64px] w-full items-center bg-black shadow-md">
             <div className="basis-1/5  text-left uppercase ml-2">Name</div>
             <div className="basis-1/5 uppercase text-left">Email</div>
@@ -28,11 +53,24 @@ const AdminActiveLabs = ({theme, setCurrentPage,fetchActiveLabs,totalPages,curre
         </div>
         {active_labs && active_labs.length > 0 ? (
           <>
-          <div className="flex flex-col w-full gap-[5px]">
-            {active_labs
-            .map((lab, index) => <ActiveLabCard lab={lab} key={index} />)}
-            </div>
-            <ThemeProvider theme={theme}>
+          {searchedActive && searchedActive.length > 0 ? 
+                (
+                <div className="flex flex-col w-full gap-[5px]">
+                    {searchedActive
+                      .map((lab, index) => <ActiveLabCard lab={lab} key={index} />)}
+                    </div>
+                )
+                :
+                (
+                <>
+                <div className="flex flex-col w-full gap-[5px]">
+                {active_labs
+                .map((lab, index) => <ActiveLabCard lab={lab} key={index} />)}
+                </div>
+                </>
+                )}
+
+          <ThemeProvider theme={theme}>
         <Stack className="basis-full flex flex-col items-center">
                 <Pagination
                 count={totalPages}
