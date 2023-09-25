0ea99425068742300ac0b7410288e8a8744de94e
marcabounader92 <marcabounader92@outlook.com>
2023-09-25 14:58:15 +0300

    Altered admin user card styling

--- hacktheweb-frontend/src/components/Cards/AdminUserCard/index.jsx
+++ hacktheweb-frontend/src/components/Cards/AdminUserCard/index.jsx
@@ -2,6 +2,7 @@ import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { restrict } from "../../../helpers/admin.helpers";
 import { setUserRestricted, setUserUnrestricted } from "../../../slices/labSlice";
+import './index.css'
 
 const AdminUsercard = ({token,user}) => {
     const navigate=useNavigate();
@@ -22,21 +23,20 @@ const AdminUsercard = ({token,user}) => {
     }
     return ( 
         <div className="flex flex-start gap-[10px] h-[64px] w-full items-center bg-black opacity-75 shadow-md">
-            <div className="basis-1/5 text-left ml-2">{user.rank}</div>
-            <div className="basis-1/5 flex-grow capitalize text-left">{user.name}</div>
-            <div className="basis-1/5 flex-grow capitalize text-left">{user.email}</div>
+            <div className="basis-[10%] text-left ml-2 parent">{user.rank}</div>
+            <div className="basis-1/5 capitalize text-left parent">{user.name}</div>
+            <div className="basis-1/5 capitalize text-left parent">{user.email}</div>
             {user.is_restricted ?
             <>
-            <div className="basis-1/5 flex-grow capitalize text-left">Restricted</div>
-            <div className="basis-1/5 flex-grow text-left"><button className="btn-2 secondary-btn" onClick={handleRestrict}>Unrestrict</button></div>
+            <div className="basis-1/5 capitalize text-left parent">Restricted</div>
+            <div className="basis-[15%] flex flex-row justify-center parent"><button className="btn-2 secondary-btn" onClick={handleRestrict}>Unrestrict</button></div>
             </>
             :
             <>
-            <div className="basis-1/5 flex-grow capitalize text-left">Not restricted</div>
-            <div className="basis-1/5 flex-grow text-left"><button className="btn-2 secondary-btn" onClick={handleRestrict}>Restrict</button></div>
+            <div className="basis-1/5 capitalize text-left parent">Not restricted</div>
+            <div className="basis-[15%] flex flex-row justify-center parent"><button className="btn-2 secondary-btn" onClick={handleRestrict}>Restrict</button></div>
             </>
             }
-            {/* <div className="basis-1/5 flex-grow text-center"><button className="btn-2 secondary-btn" >Delete</button></div> */}
         </div>
     );
 }
