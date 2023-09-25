4d0ce69cfdca901bf9b23412658de8c4a22a1cf4
marcabounader92 <marcabounader92@outlook.com>
2023-09-25 13:00:24 +0300

    Added search labs helper

---
+++ hacktheweb-frontend/src/helpers/common.helpers.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+const baseUrl = 'http://localhost:8000';
+// const baseUrl = 'http://3.248.194.204';
+
+async function searchLabs(token,query,page) {
+    try {
+      const res = await axios.get(`${baseUrl}/api/common/search-labs?page=${page}&query=${query}`, {headers: { Authorization: `Bearer ${token}` }});
+      if (res.status === 200) {
+        const data = res.data;
+        return { data };
+      }
+    } catch (error) {
+      const {
+        response: {
+          data: { message, errors },
+        },
+      } = error;
+  
+      if (errors) {
+        const errorMessages = Object.keys(errors).map((key) => {
+          const firstError = errors[key][0];
+          if (firstError) {
+            return firstError;
+          }
+        });
+        return { errorMessages };
+      }
+      return { message };
+    }
+  }
+export{searchLabs};
