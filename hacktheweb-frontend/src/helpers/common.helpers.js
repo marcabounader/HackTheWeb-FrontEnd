9849886dfc1bd7549baddcf1f093503150f0e8c1
marcabounader92 <marcabounader92@outlook.com>
2023-09-25 22:11:01 +0300

    Added search badges helper function

--- hacktheweb-frontend/src/helpers/common.helpers.js
+++ hacktheweb-frontend/src/helpers/common.helpers.js
@@ -29,4 +29,30 @@ async function searchLabs(token,query,page) {
       return { message };
     }
   }
-export{searchLabs};
+  async function searchBadges(token,query,page) {
+    try {
+      const res = await axios.get(`${baseUrl}/api/common/search-badges?page=${page}&query=${query}`, {headers: { Authorization: `Bearer ${token}` }});
+      if (res.status === 200) {
+        const data = res.data;
+        return { data };
+      }
+    } catch (error) {
+      const {
+        response: {
+          data: { message, errors },
+        },
+      } = error;
+  
+      if (errors) {
+        const errorMessages = Object.keys(errors).map((key) => {
+          const firstError = errors[key][0];
+          if (firstError) {
+            return firstError;
+          }
+        });
+        return { errorMessages };
+      }
+      return { message };
+    }
+  }
+export{searchLabs,searchBadges};
