c1f31c30df197f1b873e647271bcef99a7be8a84
marcabounader92 <marcabounader92@outlook.com>
2023-09-25 14:58:32 +0300

    Altered user card styling

--- hacktheweb-frontend/src/components/Cards/UserCard/index.jsx
+++ hacktheweb-frontend/src/components/Cards/UserCard/index.jsx
@@ -1,13 +1,13 @@
 import { useSelector } from "react-redux";
-
+import './index.css'
 const UserCard = ({user_rank}) => {
     return ( 
         <div className="flex flex-start gap-[10px] h-[64px] w-full items-center bg-black opacity-75 shadow-md">
-            <div className="basis-1/5 text-left ml-2">{user_rank.rank}</div>
-            <div className="basis-1/5 flex-grow capitalize text-left">{user_rank.name}</div>
-            <div className="basis-1/5 flex-grow text-left">{user_rank.rewards}</div>
-            <div className="basis-1/5 flex-grow text-left">{user_rank.completed_labs_count}</div>
-            <div className="basis-1/5 flex-grow text-left">{user_rank.badges_count}</div>
+            <div className="basis-1/5 text-left ml-2 parent">{user_rank.rank}</div>
+            <div className="basis-1/5 capitalize text-left parent">{user_rank.name}</div>
+            <div className="basis-1/5 text-left parent">{user_rank.rewards}</div>
+            <div className="basis-1/5 text-left parent">{user_rank.completed_labs_count}</div>
+            <div className="basis-1/5 text-left parent">{user_rank.badges_count}</div>
         </div>
     );
 }
