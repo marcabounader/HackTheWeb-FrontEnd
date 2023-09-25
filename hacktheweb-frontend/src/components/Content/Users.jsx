6e7967f664c5422df25af7c4ab2e71f5e81c5baf
marcabounader92 <marcabounader92@outlook.com>
2023-09-25 12:59:55 +0300

    Added search users and debounce

--- hacktheweb-frontend/src/components/Content/Users.jsx
+++ hacktheweb-frontend/src/components/Content/Users.jsx
@@ -1,23 +1,50 @@
 import { useEffect, useState } from "react";
 import AdminUsercard from "../Cards/AdminUserCard";
-import { Pagination, Stack, ThemeProvider } from "@mui/material";
+import { Pagination, Stack, ThemeProvider, debounce } from "@mui/material";
 import { useSelector } from "react-redux";
 
 
-const Users = ({theme,token,fetchUsers,setCurrentPage,totalPages,currentPage}) => {
+const Users = ({handleUserSearch,searchedUsers,setSearchedUsers,theme,token,fetchUsers,setCurrentPage,totalPages,currentPage}) => {
     const users = useSelector((state) => state.labs.users);
+    const debouncedHandleSearch = debounce(handleUserSearch, 300);
+    const [debouncedSearch, setDebouncedSearch] = useState("");
 
+    const handleSearchChange = (event) => {
+        const { value } = event.target;
+        setDebouncedSearch(value);
+        if (value !== "" && value !== " " && users && users.length>0) {
+            debouncedHandleSearch(value);
+        } else {
+            setSearchedUsers([]);
+        }
+      };
     const handlePageChange = (event,page) => {
         setCurrentPage(page);
-        fetchUsers();
+        if(searchedUsers && searchedUsers.length > 0){
+            handleUserSearch(debouncedSearch)
+        } else {
+            fetchUsers();
+        }
     };
     useEffect(()=>{
+        setCurrentPage(1);
         fetchUsers();
+        setSearchedUsers([]);
+        setDebouncedSearch('');
     },[])
 
     return ( 
         <>
         <h1 className="text-start self-stretch w-full">Hackers</h1>
+        <div className="w-full flex flex-row justify-center items-center">
+        <input
+                    type="search"
+                    placeholder="Search Users"
+                    value={debouncedSearch}
+                    onChange={handleSearchChange}
+        />
+        </div>
+
         <div className="flex flex-start gap-[10px] h-[64px] w-full items-center bg-black shadow-md">
             <div className="basis-1/5  text-left uppercase ml-2">Rank</div>
             <div className="basis-1/5 uppercase text-left">Name</div>
@@ -27,10 +54,23 @@ const Users = ({theme,token,fetchUsers,setCurrentPage,totalPages,currentPage}) =
         </div>
         {users && users.length > 0 ? (
             <>
-            <div className="flex flex-col w-full gap-[5px]">
-            {users
-            .map((user, index) => <AdminUsercard user={user} token={token} key={index} />)}
-            </div>
+                {searchedUsers && searchedUsers.length > 0 ? 
+                (
+                <div className="flex flex-col w-full gap-[5px]">
+                    {searchedUsers
+                    .map((user, index) => <AdminUsercard user={user} token={token} key={index} />)}
+                    </div>
+                )
+                :
+                (
+                <>
+                    <div className="flex flex-col w-full gap-[5px]">
+                    {users
+                    .map((user, index) => <AdminUsercard user={user} token={token} key={index} />)}
+                    </div>
+                </>
+                )}
+
             <ThemeProvider theme={theme}>
             <Stack className="basis-full flex flex-col items-center">
                     <Pagination
