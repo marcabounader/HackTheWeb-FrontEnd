ed283ee2effcd97fb5bb922f0c095fa1f6a01070
marcabounader92 <marcabounader92@outlook.com>
2023-09-26 18:33:40 +0300

    Added prompt engineering for handle request chatbot

--- hacktheweb-frontend/src/components/Popover/ChatBot/ActionProvider.js
+++ hacktheweb-frontend/src/components/Popover/ChatBot/ActionProvider.js
@@ -18,17 +18,25 @@ const ActionProvider = ({ createChatBotMessage, setState, children }) => {
     }));
   };
   const handleRequest = async (request) => {
-    const newMessage = `${messages}\n${request}`;
-    setMessage(newMessage)
-    console.log(newMessage);
-    const {data,message,errorMessages} = await getBotResponse(token,newMessage);
+    let initialMessage = "This message is being sent using HackTheWeb Assistant to aid the user on how to implement web app attacks.";
+    initialMessage += "\nThe user will be asking questions related to the app.";
+    initialMessage += "\nThe app contains different labs for implementing different web attacks according to OWASP standards.";
+    initialMessage += "\nThe app is using Docker to run instances of OWASP Mutillidae 2.";
+    initialMessage += "\nEach instance is a different attack scenario.";
+    initialMessage += "\nAnswer the question as if you are HackTheWeb Assistant.";
+    initialMessage += "\nThe user is asking the following questions:";
+  
+    const newMessage = `${initialMessage}\n${messages}\n${request}`;
+    setMessage(`${messages}\n${request}`);
+    const { data, message, errorMessages } = await getBotResponse(token, newMessage);
     const botMessage = createChatBotMessage(data.response);
-    
+  
     setState((prev) => ({
       ...prev,
       messages: [...prev.messages, botMessage],
     }));
   };
+  
 
   // Put the handleHello function in the actions object to pass to the MessageParser
   return (
