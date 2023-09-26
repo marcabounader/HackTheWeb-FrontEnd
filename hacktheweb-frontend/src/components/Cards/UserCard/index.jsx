06b42dc257de4d7034e3bd92761abc8b10b1758f
marcabounader92 <marcabounader92@outlook.com>
2023-09-26 20:12:01 +0300

    Aldded classname to user card

--- hacktheweb-frontend/src/components/Cards/UserCard/index.jsx
+++ hacktheweb-frontend/src/components/Cards/UserCard/index.jsx
@@ -2,12 +2,12 @@ import { useSelector } from "react-redux";
 import './index.css'
 const UserCard = ({user_rank}) => {
     return ( 
-        <div className="flex flex-start gap-[10px] h-[64px] w-full items-center bg-black opacity-75 shadow-md">
-            <div className="basis-1/5 text-left ml-2 parent">{user_rank.rank}</div>
-            <div className="basis-1/5 capitalize text-left parent">{user_rank.name}</div>
-            <div className="basis-1/5 text-left parent">{user_rank.rewards}</div>
-            <div className="basis-1/5 text-left parent">{user_rank.completed_labs_count}</div>
-            <div className="basis-1/5 text-left parent">{user_rank.badges_count}</div>
+        <div className="user-card-wrapper flex flex-start gap-[10px] h-[64px] w-full items-center bg-black opacity-75 shadow-md">
+            <div className="basis-1/5 ml-2 parent">{user_rank.rank}</div>
+            <div className="basis-1/5 capitalize parent">{user_rank.name}</div>
+            <div className="basis-1/5 parent">{user_rank.rewards ? user_rank.rewards : "0"}</div>
+            <div className="basis-1/5 parent">{user_rank.completed_labs_count}</div>
+            <div className="basis-1/5 parent">{user_rank.badges_count}</div>
         </div>
     );
 }
