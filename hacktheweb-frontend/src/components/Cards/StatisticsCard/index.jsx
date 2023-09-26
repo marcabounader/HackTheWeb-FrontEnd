b057372f7b47d7f4b8ab7993ec1f7664ec520504
marcabounader92 <marcabounader92@outlook.com>
2023-09-26 20:11:50 +0300

    Added default statistic value

--- hacktheweb-frontend/src/components/Cards/StatisticsCard/index.jsx
+++ hacktheweb-frontend/src/components/Cards/StatisticsCard/index.jsx
@@ -2,7 +2,7 @@ const StatisticsCard = ({name,statistic}) => {
     return ( 
         <div className="flex w-[352px] h-[157px] p-[10px] flex-col justify-around items-center gap-[10px] shadow-md bg-bg-card rounded-[10px]">
             <h4>{name}</h4>
-            <p className=" text-5xl">{statistic}</p>
+            <p className=" text-5xl">{statistic ? statistic : "0"}</p>
         </div>       
     );
 }
