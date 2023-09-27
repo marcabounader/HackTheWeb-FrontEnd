81cb3bc3ae266b6e6e4c5f56bd019d3fb4e8d5bb
marcabounader92 <marcabounader92@outlook.com>
2023-09-27 13:25:58 +0300

    Added overflow scroll user card

--- hacktheweb-frontend/src/components/Cards/UserCard/index.jsx
+++ hacktheweb-frontend/src/components/Cards/UserCard/index.jsx
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux";
 import './index.css'
 const UserCard = ({user_rank}) => {
     return ( 
-        <div className="user-card-wrapper flex flex-start gap-[10px] h-[64px] w-full items-center bg-black opacity-75 shadow-md">
+        <div className="user-card-wrapper flex flex-start gap-[10px] h-[64px] w-full items-center bg-black opacity-75 shadow-md overflow-x-auto">
             <div className="basis-1/5 ml-2 parent">{user_rank.rank}</div>
             <div className="basis-1/5 capitalize parent">{user_rank.name}</div>
             <div className="basis-1/5 parent">{user_rank.rewards ? user_rank.rewards : "0"}</div>
