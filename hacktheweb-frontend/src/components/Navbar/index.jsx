d2a4f8311ee692dba81061af250713dc5a9c7b29
marcabounader92 <marcabounader92@outlook.com>
2023-09-27 13:27:50 +0300

    Added menu button css and removed unecessary gap

--- hacktheweb-frontend/src/components/Navbar/index.jsx
+++ hacktheweb-frontend/src/components/Navbar/index.jsx
@@ -79,7 +79,7 @@ const Navbar = ({onEnter,onLeave, onPrimaryEnter, onPrimaryLeave, handleOpenLogi
                 :
                     (
                         <>
-                        <div className='menu-container flex flex-row'>
+                        <div className='menu-container flex flex-row w-[44px] h-[44px] flex-grow justify-end items-center'>
                             <Button 
                             id="basic-btton"
                             aria-controls={open ? 'basic-menu' : undefined}
@@ -90,11 +90,11 @@ const Navbar = ({onEnter,onLeave, onPrimaryEnter, onPrimaryLeave, handleOpenLogi
                             {
                                 profile_url ?
                                 (
-                                    <img  className='rounded-full' src={profile_url} alt="user image"/>
+                                    <img  className='rounded-full profile-image' src={profile_url} alt="user image"/>
                                 )
                                 :
                                 (
-                                    <svg className='rounded-full bg-white' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none">
+                                    <svg className='rounded-full bg-white profile-image' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none">
                                         <g clipPath="url(#clip0_156_5270)">
                                             <path d="M12 13.5C15.7266 13.5 18.75 10.4766 18.75 6.75C18.75 3.02344 15.7266 0 12 0C8.27344 0 5.25 3.02344 5.25 6.75C5.25 10.4766 8.27344 13.5 12 13.5ZM18 15H15.4172C14.3766 15.4781 13.2188 15.75 12 15.75C10.7812 15.75 9.62813 15.4781 8.58281 15H6C2.68594 15 0 17.6859 0 21V21.75C0 22.9922 1.00781 24 2.25 24H21.75C22.9922 24 24 22.9922 24 21.75V21C24 17.6859 21.3141 15 18 15Z" fill="black"/>
                                         </g>
@@ -119,7 +119,7 @@ const Navbar = ({onEnter,onLeave, onPrimaryEnter, onPrimaryLeave, handleOpenLogi
                             }}
                             PopoverClasses={{paper: 'custom-popover'}}
                             >
-                                <MenuItem onClick={() => {toggleContent("leaderboard");}}>Leaderboard</MenuItem>
+                            <MenuItem onClick={() => {toggleContent("leaderboard");}}>Leaderboard</MenuItem>
                                 {menu_items}
                             </Menu>
                         </div>
@@ -127,7 +127,7 @@ const Navbar = ({onEnter,onLeave, onPrimaryEnter, onPrimaryLeave, handleOpenLogi
                                   
                             <button className="btn" onMouseEnter={onEnter} onMouseLeave={onLeave} onClick={() => {toggleContent("leaderboard");}}>Leaderboard</button>
                             <ProfileBar handleOpenPassword={handleOpenPassword} handleOpenProfile={handleOpenProfile} onMouse={onPrimaryEnter} onLeave={onPrimaryLeave} onPrimaryEnter={onPrimaryEnter} onPrimaryLeave={onPrimaryLeave}/>
-                            <div className=' flex w-[44px] h-[44px] ml-3 gap-[10px] justify-center self-center items-center rounded-full'>
+                            <div className=' flex w-[44px] h-[44px] ml-3 justify-center self-center items-center rounded-full'>
                             <>
                             {
                                 profile_url ?
