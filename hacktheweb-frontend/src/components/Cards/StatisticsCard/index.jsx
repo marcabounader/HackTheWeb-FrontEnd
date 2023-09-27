b52cb12b136ad6f5bb3de1ee2313459c4be06d84
marcabounader92 <marcabounader92@outlook.com>
2023-09-27 12:43:36 +0300

    Added card statistics classname

--- hacktheweb-frontend/src/components/Cards/StatisticsCard/index.jsx
+++ hacktheweb-frontend/src/components/Cards/StatisticsCard/index.jsx
@@ -1,6 +1,6 @@
 const StatisticsCard = ({name,statistic}) => {
     return ( 
-        <div className="flex w-[352px] h-[157px] p-[10px] flex-col justify-around items-center gap-[10px] shadow-md bg-bg-card rounded-[10px]">
+        <div className=".statistics-card-wrapper flex basis-1/5 p-[10px] self-stretch flex-col justify-around items-center gap-[10px] shadow-md bg-bg-card rounded-[10px]">
             <h4>{name}</h4>
             <p className=" text-5xl">{statistic ? statistic : "0"}</p>
         </div>       
