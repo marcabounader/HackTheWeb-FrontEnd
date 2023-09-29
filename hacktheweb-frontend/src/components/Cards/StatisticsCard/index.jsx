96062ce89906482bee90fe2f1b5feb0f8ed60e2a
marcabounader92 <marcabounader92@outlook.com>
2023-09-29 17:39:24 +0300

    Changed statistic card basis

--- hacktheweb-frontend/src/components/Cards/StatisticsCard/index.jsx
+++ hacktheweb-frontend/src/components/Cards/StatisticsCard/index.jsx
@@ -1,6 +1,6 @@
 const StatisticsCard = ({name,statistic}) => {
     return ( 
-        <div className="statistics-card-wrapper flex basis-1/5 p-[10px] self-stretch flex-col justify-around items-center gap-[10px] shadow-md bg-bg-card rounded-[10px]">
+        <div className="statistics-card-wrapper flex basis-[23%] p-[10px] self-stretch flex-col justify-around items-center gap-[10px] shadow-md bg-bg-card rounded-[10px]">
             <h4>{name}</h4>
             <p className=" text-5xl">{statistic ? statistic : "0"}</p>
         </div>       
