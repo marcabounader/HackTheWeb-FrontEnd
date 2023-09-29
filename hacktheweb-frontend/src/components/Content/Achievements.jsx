19d8a2f25389734baf12289fea7dc149526ccce9
marcabounader92 <marcabounader92@outlook.com>
2023-09-29 17:39:44 +0300

    Altered achivements search bar location

--- hacktheweb-frontend/src/components/Content/Achievements.jsx
+++ hacktheweb-frontend/src/components/Content/Achievements.jsx
@@ -47,28 +47,32 @@ const Achievements = ({handleBadgeSearch,theme,setCurrentPage,totalPages,current
     return ( 
         <>
         <AddBadgeModal isOpen={showAddBadge} handleCloseViewModal={handleCloseAddBadge} token={token}/>
+        <div className=" basis-full flex-wrap flex flex-row justify-between items-center">
         { type_id=="3" ?
         <>
-        <h1 className="text-start w-full">Achievements</h1>
-        </>
-        :
-        <>
-        <div className="flex flex-row basis-full justify-between items-center">
-        <h1 className="text-start">Badges</h1>
-        <FontAwesomeIcon onClick={handleOpenAddBadge} icon={faPlusSquare} className="text-color-secondary w-[40px] h-[40px]" ></FontAwesomeIcon>
-        </div>
-        </>
-        }
-        {badges && badges.length > 0 ? (
-          <>
-          <div className="w-full flex flex-row justify-center items-center">
+        <h1 className="text-start">Achievements</h1>
         <input
                     type="search"
                     placeholder="Search Badges"
                     value={debouncedSearch}
                     onChange={handleSearchChange}
         />
+        </>
+        :
+        <>
+        <h1 className="text-start">Badges</h1>
+        <input
+                    type="search"
+                    placeholder="Search Badges"
+                    value={debouncedSearch}
+                    onChange={handleSearchChange}
+        />
+        <FontAwesomeIcon onClick={handleOpenAddBadge} icon={faPlusSquare} className="text-color-secondary w-[40px] h-[40px]" ></FontAwesomeIcon>
+        </>
+        }
         </div>
+        {badges && badges.length > 0 ? (
+          <>
         {badges.map((badge, index) => 
         <BadgeCard badges={badges} type_id={type_id} badge={badge} key={index} token={token} />
         )}
