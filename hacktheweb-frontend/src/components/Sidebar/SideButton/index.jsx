035dd0c1c8e571fcd81786c7c3b64b3ae53666d9
marcabounader92 <marcabounader92@outlook.com>
2023-09-29 17:42:40 +0300

    Changed side button class

--- hacktheweb-frontend/src/components/Sidebar/SideButton/index.jsx
+++ hacktheweb-frontend/src/components/Sidebar/SideButton/index.jsx
@@ -2,7 +2,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const SideButton = ({onEnter,onLeave, icon, text, onClick,className, fill, icon_style=null}) => {
   return (
-    <div className={`self-stretch items-center flex gap-[10px] h-[60px] cursor-pointer ${className}`} onClick={onClick} onMouseEnter={onEnter} onMouseLeave={onLeave}>
+    <div className={`side-button-wrapper self-stretch items-center flex gap-[10px] h-[60px] cursor-pointer ${className}`} onClick={onClick} onMouseEnter={onEnter} onMouseLeave={onLeave}>
       {icon && (
         <div>
           <FontAwesomeIcon icon={icon} width="35px" height="35px" className={icon_style}/>
