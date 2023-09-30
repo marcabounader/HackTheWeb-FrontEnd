2c8a6df9dc49fd35dab98a5013aadd25265aa40a
marcabounader92 <marcabounader92@outlook.com>
2023-09-30 18:26:02 +0300

    Added users items per page selector

--- hacktheweb-frontend/src/components/Content/Users.jsx
+++ hacktheweb-frontend/src/components/Content/Users.jsx
@@ -4,11 +4,13 @@ import { Pagination, Stack, ThemeProvider, debounce } from "@mui/material";
 import { useSelector } from "react-redux";
 
 
-const Users = ({handleUserSearch,theme,token,setCurrentPage,totalPages,currentPage}) => {
+const Users = ({usersPerPage,setUsersPerPage,handleUserSearch,theme,token,setCurrentPage,totalPages,currentPage}) => {
     const users = useSelector((state) => state.labs.users);
     const [debounceTimer, setDebounceTimer] = useState(null);
     const [debouncedSearch, setDebouncedSearch] = useState("");
-
+    const handleItemsPerPageChange = (event, value) => {
+        setUsersPerPage(value);
+      };
     const handleSearchChange = (event) => {
         const { value } = event.target;
         setDebouncedSearch(value);
@@ -36,7 +38,9 @@ const Users = ({handleUserSearch,theme,token,setCurrentPage,totalPages,currentPa
         handleUserSearch('');
         setDebouncedSearch('');
     },[])
-
+    useEffect(()=>{
+        handleUserSearch(debouncedSearch);
+      },[usersPerPage]);
     return ( 
         <>
         <div className=" basis-full flex flex-row justify-between items-center">
@@ -64,17 +68,24 @@ const Users = ({handleUserSearch,theme,token,setCurrentPage,totalPages,currentPa
                 .map((user, index) => <AdminUsercard user={user} token={token} key={index} />)}
             </div>
 
+            <div className="basis-full flex flex-row justify-center items-center">
             <ThemeProvider theme={theme}>
-            <Stack className="basis-full flex flex-col items-center">
+            <Stack>
                     <Pagination
                     count={totalPages}
                     page={currentPage}
                     onChange={handlePageChange}
                     color="primary"
-                    
                     />
             </Stack>
             </ThemeProvider>
+            <select className=" rounded-none text-black" value={usersPerPage} onChange={(e) => handleItemsPerPageChange(e, e.target.value)}>
+            <option value={5}>5 per page</option>
+            <option value={10}>10 per page</option>
+            <option value={15}>15 per page</option>
+            <option value={20}>20 per page</option>
+            </select>
+            </div>
             </>
         ) : (
           <p>No Users.</p>
