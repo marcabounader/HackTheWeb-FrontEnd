362dd373096ba138c8d23f5b14828378c7f211fe
marcabounader92 <marcabounader92@outlook.com>
2023-09-30 19:22:49 +0300

    Increased lab modal height

--- hacktheweb-frontend/src/components/Modals/LabModal.jsx
+++ hacktheweb-frontend/src/components/Modals/LabModal.jsx
@@ -145,7 +145,7 @@ const LabModal = ({isStopLoading,handleOpenStopConfirmation,isOpen,handleCloseVi
         <Modal
         isOpen={isOpen}
         onRequestClose={handleCloseViewModal}
-        className="z-30 overflow-y-auto bg-[#0D1115] w-[50%] h-[70%] transition-opacity fixed top-1/2 left-1/2 transform  -translate-x-1/2 -translate-y-1/2 dark:border flex flex-col items-center gap-3 shadow-lg py-[10px] px-[20px]"
+        className="z-30 overflow-y-auto bg-[#0D1115] w-[50%] h-[80%] transition-opacity fixed top-1/2 left-1/2 transform  -translate-x-1/2 -translate-y-1/2 dark:border flex flex-col items-center gap-3 shadow-lg py-[10px] px-[20px]"
         overlayClassName="fixed top-0 z-10 left-0 w-[100%] h-full backdrop-blur-xl drop-shadow-lg"
         >   
             <div className='lab-title flex justify-between items-center self-stretch'>
